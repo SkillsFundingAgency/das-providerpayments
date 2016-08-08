b9b202ba5650681c7e7e4463b2eec99c8e36b11d
Simon Bull <simon.bull@taurumconsulting.com>
2016-08-08 16:56:43 +0100

    Add solution

---
+++ src/SFA.DAS.ProviderPayments.Api.UnitTests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace SFA.DAS.ProviderPayments.Api.UnitTests
+{
+    public class Class1
+    {
+        [Test]
+        public void IsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
