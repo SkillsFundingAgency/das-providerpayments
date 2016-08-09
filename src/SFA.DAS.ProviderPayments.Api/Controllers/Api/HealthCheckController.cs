8e94d7d1a78ccec90c9b6b1f9937550557c6fef7
Simon Bull <simon.bull@taurumconsulting.com>
2016-08-09 09:55:39 +0100

    Add scaffolding for solution
    * Add domain project
    * Add infrastructure project
    * Add application project
    * Install StuctureMap packages into Api project

---
+++ src/SFA.DAS.ProviderPayments.Api/Controllers/Api/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SFA.DAS.ProviderPayments.Api.Controllers.Api
+{
+    public class HealthCheckController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
