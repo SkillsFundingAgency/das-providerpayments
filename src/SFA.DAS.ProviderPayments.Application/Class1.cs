7320e88a847563636a53c1d28c8fbb8588683882
Simon Bull <sbulluk@users.noreply.github.com>
2016-08-09 14:57:06 +0100

    Remove unused directives across solution

--- src/SFA.DAS.ProviderPayments.Application/Class1.cs
+++ src/SFA.DAS.ProviderPayments.Application/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SFA.DAS.ProviderPayments.Application
+﻿namespace SFA.DAS.ProviderPayments.Application
 {
     public class Class1
     {
