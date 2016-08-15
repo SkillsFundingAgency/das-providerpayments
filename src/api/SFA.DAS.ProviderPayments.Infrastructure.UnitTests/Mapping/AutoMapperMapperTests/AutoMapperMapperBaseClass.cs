43dd31a527bad46c8a964949bba235dfac028c53
Simon Bull <sbulluk@users.noreply.github.com>
2016-08-15 16:24:05 +0100

    Add tests for AutoMapperMapper

---
+++ src/api/SFA.DAS.ProviderPayments.Infrastructure.UnitTests/Mapping/AutoMapperMapperTests/AutoMapperMapperBaseClass.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace SFA.DAS.ProviderPayments.Infrastructure.UnitTests.Mapping.AutoMapperMapperTests
+{
+    public abstract class AutoMapperMapperBaseClass
+    {
+        protected AutoMapper.MapperConfiguration CreateConfiguration()
+        {
+            return new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<TestDomainObject, TestDtoObject>();
+            });
+        }
+
+        protected void AssertMappingIsCorrect(TestDomainObject source, TestDtoObject destination)
+        {
+            Assert.AreEqual(source.Id, destination.Id);
+            Assert.AreEqual(source.Description, destination.Description);
+        }
+    }
+}
