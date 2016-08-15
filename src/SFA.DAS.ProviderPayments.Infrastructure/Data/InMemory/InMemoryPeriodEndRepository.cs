f1a22b76d71fbfa0574d6a14a975cddc9eaf391b
Simon Bull <sbulluk@users.noreply.github.com>
2016-08-15 13:05:04 +0100

    Add in-memory period end repository

---
+++ src/SFA.DAS.ProviderPayments.Infrastructure/Data/InMemory/InMemoryPeriodEndRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.ProdiverPayments.Domain.Data;
+using SFA.DAS.ProdiverPayments.Domain.Data.Entities;
+
+namespace SFA.DAS.ProdiverPayments.Infrastructure.Data.InMemory
+{
+    public class InMemoryPeriodEndRepository : IPeriodEndRepository
+    {
+        private const int PageSize = 10;
+
+        private readonly List<PeriodEndEntity> _periodEnds;
+
+        public InMemoryPeriodEndRepository()
+        {
+            _periodEnds = new List<PeriodEndEntity>
+            {
+                new PeriodEndEntity { PeriodCode = "201704" }
+            };
+        }
+
+        public Task<PageOfEntities<PeriodEndEntity>> GetPageAsync(int pageNumber)
+        {
+            var skip = (pageNumber - 1) * PageSize;
+            var items = _periodEnds.Skip(skip).Take(PageSize).ToArray();
+            var page = new PageOfEntities<PeriodEndEntity>
+            {
+                Items = items,
+                TotalNumberOfItems = _periodEnds.Count,
+                TotalNumberOfPages = (int)Math.Ceiling(_periodEnds.Count / (float)PageSize)
+            };
+
+            return Task.FromResult(page);
+        }
+    }
+}
