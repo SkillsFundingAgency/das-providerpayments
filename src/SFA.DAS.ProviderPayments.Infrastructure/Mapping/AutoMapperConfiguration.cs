e2807af3518b3cf975a9154e1a02625c9a57e7ab
Simon Bull <sbulluk@users.noreply.github.com>
2016-08-15 11:49:07 +0100

    Add AutoMapper

---
+++ src/SFA.DAS.ProviderPayments.Infrastructure/Mapping/AutoMapperConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using SFA.DAS.ProdiverPayments.Domain;
+using SFA.DAS.ProdiverPayments.Domain.Data.Entities;
+
+namespace SFA.DAS.ProdiverPayments.Infrastructure.Mapping
+{
+    public static class AutoMapperConfiguration
+    {
+        public static MapperConfiguration Configure()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                AddPeriodEndMappings(cfg);
+            });
+        }
+
+        private static void AddPeriodEndMappings(IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<PeriodEndEntity, PeriodEnd>();
+        }
+    }
+}
---
+++ src/SFA.DAS.ProviderPayments.Infrastructure/Mapping/AutoMapperMapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace SFA.DAS.ProdiverPayments.Infrastructure.Mapping
+{
+    public class AutoMapperMapper : Domain.Mapping.IMapper
+    {
+        private IMapper _mapper;
+
+        public AutoMapperMapper(MapperConfiguration mapperConfiguration)
+        {
+            _mapper = mapperConfiguration.CreateMapper();
+        }
+
+        public TDestination Map<TSource, TDestination>(TSource source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TDestination> Map<TSource, TDestination>(IEnumerable<TSource> source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
