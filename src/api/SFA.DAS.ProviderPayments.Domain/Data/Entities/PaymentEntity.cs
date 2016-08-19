99d7620042ae69e5f7a13f5a348c3ef2c679b418
Simon Bull <sbulluk@users.noreply.github.com>
2016-08-19 12:36:17 +0100

    Add in memory repository for payments

--- src/api/SFA.DAS.ProviderPayments.Domain/Data/Entities/PaymentEntity.cs
+++ src/api/SFA.DAS.ProviderPayments.Domain/Data/Entities/PaymentEntity.cs
@@ -11,8 +11,12 @@ namespace SFA.DAS.ProviderPayments.Domain.Data.Entities
         public string AccountId { get; set; }
         public string Ukprn { get; set; }
         public long Uln { get; set; }
+        public long StandardCode { get; set; }
+        public int PathwayCode { get; set; }
+        public int FrameworkCode { get; set; }
+        public int ProgrammeType { get; set; }
         public string ReportedPeriodCode { get; set; }
-        public string DeliveryPeriod { get; set; }
+        public string DeliveryPeriodCode { get; set; }
         public decimal Amount { get; set; }
         public int TransactionType { get; set; }
         public int FundingType { get; set; }
---
+++ src/api/SFA.DAS.ProviderPayments.Infrastructure/Data/InMemory/InMemoryPaymentRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.ProviderPayments.Domain.Data;
+using SFA.DAS.ProviderPayments.Domain.Data.Entities;
+
+namespace SFA.DAS.ProviderPayments.Infrastructure.Data.InMemory
+{
+    public class InMemoryPaymentRepository : IPaymentRepository
+    {
+        private PaymentEntity[] _payments;
+        private const int PageSize = 10;
+
+        public InMemoryPaymentRepository()
+        {
+            _payments = new[]
+            {
+                new PaymentEntity
+                {
+                    AccountId = "DasAccount1",
+                    Ukprn = "PROV1",
+                    Uln = 12344532,
+                    StandardCode = 8348957394,
+                    ReportedPeriodCode = "201704",
+                    DeliveryPeriodCode = "201704",
+                    Amount = 134.31m,
+                    TransactionType = 0,
+                    FundingType = 0
+                }
+            };
+        }
+
+        public Task<PageOfEntities<PaymentEntity>> GetPageOfPaymentsForAccountInPeriod(string periodCode, string accountId, int pageNumber)
+        {
+            var skip = (pageNumber - 1) * PageSize;
+            var payments = _payments.Where(p => p.ReportedPeriodCode == periodCode && p.AccountId.Equals(accountId, StringComparison.OrdinalIgnoreCase))
+                .ToArray();
+            var pageOfPayments = payments.Skip(skip).Take(PageSize).ToArray();
+            if (pageOfPayments == null || !pageOfPayments.Any())
+            {
+                return Task.FromResult<PageOfEntities<PaymentEntity>>(null);
+            }
+
+            return Task.FromResult(new PageOfEntities<PaymentEntity>
+            {
+                TotalNumberOfItems = payments.Length,
+                TotalNumberOfPages = (int)Math.Ceiling(payments.Length / (float)PageSize),
+                Items = pageOfPayments
+            });
+        }
+    }
+}
