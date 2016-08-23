761d4c37f23f41f8a677bffbb17a9c9e63bccb94
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-08-23 13:09:28 +0100

    Update dc component packaging batch script.

--- src/calculator/RunPackComponent.bat
+++ src/calculator/RunPackComponent.bat
@@ -2,4 +2,8 @@ if exist "Deploy\component" (
   rd /s /q "Deploy\component"
 )
 
+if not exist "Deploy\component" (
+  md "Deploy\component"
+)
+
 robocopy SFA.DAS.ProviderPayments.Calculator.LevyPayments\bin\Release\ Deploy\component\ *.dll
