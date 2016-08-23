58338c9ccb7f700b381e35cf09fce7024d343b70
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-08-23 12:09:42 +0100

    Pick up DC component DLLs to be packed from the Release folder rather than the Debug folder.

--- src/calculator/RunPackComponent.bat
+++ src/calculator/RunPackComponent.bat
@@ -2,4 +2,4 @@ if exist "Deploy\component" (
   rd /s /q "Deploy\component"
 )
 
-robocopy SFA.DAS.ProviderPayments.Calculator.LevyPayments\bin\Debug\ Deploy\component\ *.dll
+robocopy SFA.DAS.ProviderPayments.Calculator.LevyPayments\bin\Release\ Deploy\component\ *.dll
