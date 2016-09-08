d346284e1ae3d6204ec40c2363c0539b6bf97cc1
Simon Bull <sbulluk@users.noreply.github.com>
2016-09-08 15:54:52 +0100

    Create payment schedule project and start to abstract common task logic

---
+++ src/calculator/SFA.DAS.ProviderPayments.Calculator.Common/Context/ContextPropertyKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.ProviderPayments.Calculator.Common.Context
+{
+    public class ContextPropertyKeys
+    {
+        public const string TransientDatabaseConnectionString = "TransientDatabaseConnectionString";
+        public const string LogLevel = "LogLevel";
+    }
+}
---
+++ src/calculator/SFA.DAS.ProviderPayments.Calculator.Common/DcfsTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS.Common.External.Interfaces;
+using SFA.DAS.ProviderPayments.Calculator.Common.Context;
+
+namespace SFA.DAS.ProviderPayments.Calculator.Common
+{
+    public abstract class DcfsTask : IExternalTask
+    {
+        public virtual void Execute(IExternalContext context)
+        {
+            var contextWrapper = GetContextWrapper(context);
+            ValidateContext(contextWrapper);
+
+            SetupLogging(contextWrapper);
+        }
+
+        protected virtual ContextWrapper GetContextWrapper(IExternalContext context)
+        {
+            return new ContextWrapper(context);
+        }
+        protected virtual void ValidateContext(ContextWrapper contextWrapper)
+        {
+            if (string.IsNullOrEmpty(contextWrapper.GetPropertyValue(ContextPropertyKeys.TransientDatabaseConnectionString)))
+            {
+                throw new InvalidContextException(InvalidContextException.ContextPropertiesNoConnectionStringMessage);
+            }
+
+            if (string.IsNullOrEmpty(contextWrapper.GetPropertyValue(ContextPropertyKeys.LogLevel)))
+            {
+                throw new InvalidContextException(InvalidContextException.ContextPropertiesNoLogLevelMessage);
+            }
+        }
+
+        protected virtual void SetupLogging(ContextWrapper contextWrapper)
+        {
+        }
+    }
+}
