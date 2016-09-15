49a16e4eb1c4254b94aec2bd3fe8feefb154b0b5
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-09-15 11:21:12 +0100

    Add previously ignored files due to the gitignore settings.

---
+++ src/calculator/SFA.DAS.ProviderPayments.Calc.Common/Tools/Extensions/DateTimeDayOfMonthExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SFA.DAS.ProviderPayments.Calc.Common.Tools.Extensions
+{
+    public static class DateTimeDayOfMonthExtensions
+    {
+        public static int DaysInMonth(this DateTime value)
+        {
+            return DateTime.DaysInMonth(value.Year, value.Month);
+        }
+
+        public static DateTime LastDayOfMonth(this DateTime value)
+        {
+            return new DateTime(value.Year, value.Month, value.DaysInMonth());
+        }
+    }
+}
