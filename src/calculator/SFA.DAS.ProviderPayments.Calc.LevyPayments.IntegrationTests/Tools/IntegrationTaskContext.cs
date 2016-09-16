c16e408e3f4804a7daafaba82b11565b253ea862
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-09-16 11:27:04 +0100

    Refactor database integration tests.

---
+++ src/calculator/SFA.DAS.ProviderPayments.Calc.LevyPayments.IntegrationTests/Tools/IntegrationTaskContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using CS.Common.External.Interfaces;
+using SFA.DAS.ProviderPayments.Calc.Common.Context;
+
+namespace SFA.DAS.ProviderPayments.Calc.LevyPayments.IntegrationTests.Tools
+{
+    public class IntegrationTaskContext : IExternalContext
+    {
+        public IntegrationTaskContext()
+        {
+            Properties = new Dictionary<string, string>
+            {
+                {ContextPropertyKeys.TransientDatabaseConnectionString, GlobalTestContext.Instance.ConnectionString},
+                {ContextPropertyKeys.LogLevel, "DEBUG"}
+            };
+        }
+
+        public IDictionary<string, string> Properties { get; set; }
+    }
+}
