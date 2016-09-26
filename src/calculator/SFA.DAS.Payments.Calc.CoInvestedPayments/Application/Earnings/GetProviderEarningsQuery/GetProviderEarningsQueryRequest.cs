b8040705d97167c3e72d029c989212d714b7a878
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-09-26 13:45:09 +0100

    Remove GetProviderEarningsQuery and related unit tests. Move DDL scripts to the 'DeployCoInvestedPayments' folder and link reference them in the solution. Remove not needed views related to earnings and commitments.

--- src/calculator/SFA.DAS.Payments.Calc.CoInvestedPayments/Application/Earnings/GetProviderEarningsQuery/GetProviderEarningsQueryRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.Payments.Calc.CoInvestedPayments.Application.Earnings.GetProviderEarningsQuery
-{
-    public class GetProviderEarningsQueryRequest : IRequest<GetProviderEarningsQueryResponse>
-    {
-        public long Ukprn { get; set; }
-    }
-}
