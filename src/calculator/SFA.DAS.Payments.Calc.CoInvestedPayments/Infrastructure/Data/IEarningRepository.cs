1d135a4337beb9b49068802ddd2491e924096565
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-09-26 13:48:16 +0100

    Remove Earnings related repository and entity.

--- src/calculator/SFA.DAS.Payments.Calc.CoInvestedPayments/Infrastructure/Data/IEarningRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using SFA.DAS.Payments.Calc.CoInvestedPayments.Infrastructure.Data.Entities;
-
-namespace SFA.DAS.Payments.Calc.CoInvestedPayments.Infrastructure.Data
-{
-    public interface IEarningRepository
-    {
-        EarningEntity[] GetProviderEarnings(long ukprn);
-    }
-}
