ad004520ad0a7c85483b53885253a18e544464b3
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-09-26 11:51:14 +0100

    Update DC component pack batch script to work with the new name for the Co-Invested Payments component.

--- src/calculator/RunPackComponent.bat
+++ src/calculator/RunPackComponent.bat
@@ -7,7 +7,14 @@ if exist "Deploy%Component%\test-results" ( rd /s /q "Deploy%Component%\test-res
 if not exist "Deploy%Component%\component" ( md "Deploy%Component%\component" )
 if not exist "Deploy%Component%\test-results" ( md "Deploy%Component%\test-results" )
 
-xcopy SFA.DAS.ProviderPayments.Calc.%Component%\bin\%BuildConfig%\*.dll Deploy%Component%\component\
+if exist "SFA.DAS.ProviderPayments.Calc.%Component%\bin\%BuildConfig%" (
+  xcopy SFA.DAS.ProviderPayments.Calc.%Component%\bin\%BuildConfig%\*.dll Deploy%Component%\component\
+)
+
+if exist "SFA.DAS.Payments.Calc.%Component%\bin\%BuildConfig%" (
+  xcopy SFA.DAS.Payments.Calc.%Component%\bin\%BuildConfig%\*.dll Deploy%Component%\component\
+)
+
 xcopy TestResult*.Common.xml Deploy%Component%\test-results\
 xcopy TestResult*.%Component%.xml Deploy%Component%\test-results\
 
