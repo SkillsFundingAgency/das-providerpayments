8c90a93bcba2a565b9b86a3b69f4e38c850c3a5e
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-09-26 16:20:15 +0100

    Remove ProcessPaymentCommand and associated unit tests.

--- src/calculator/SFA.DAS.Payments.Calc.CoInvestedPayments/Application/Payments/ProcessPaymentCommand/ProcessPaymentCommandRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.Payments.Calc.CoInvestedPayments.Application.Payments.ProcessPaymentCommand
-{
-    public class ProcessPaymentCommandRequest : IRequest
-    {
-        public Payment Payment { get; set; }
-    }
-}
