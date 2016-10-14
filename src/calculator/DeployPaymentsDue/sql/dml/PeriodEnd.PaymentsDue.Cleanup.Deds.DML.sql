dc95ac01bbeed057f4ca9221ebcb01167c9c0bca
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-10-14 16:45:17 +0100

    Update Payments/Earnings Due deds cleanup script.

--- src/calculator/DeployPaymentsDue/sql/dml/PeriodEnd.PaymentsDue.Cleanup.Deds.DML.sql
+++ src/calculator/DeployPaymentsDue/sql/dml/PeriodEnd.PaymentsDue.Cleanup.Deds.DML.sql
@@ -1,5 +1,5 @@
 DELETE FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[RequiredPayments]
-    WHERE [Ukprn] IN (SELECT [Ukprn] FROM ${ILR_Deds.FQ}.[Valid].[LearningProvider])
+    WHERE [Ukprn] IN (SELECT DISTINCT [Ukprn] FROM [PaymentsDue].[RequiredPayments])
         AND [CollectionPeriodName] IN (SELECT [Collection_Period] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
         AND [CollectionPeriodMonth] IN (SELECT [Period] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
         AND [CollectionPeriodYear] IN (SELECT [Calendar_Year] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
