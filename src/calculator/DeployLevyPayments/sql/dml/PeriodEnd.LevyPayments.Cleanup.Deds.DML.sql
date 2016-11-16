4f96ee8d3d406b1a38d4545a033deb6f26730102
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-11-16 16:40:15 +0000

    Remove ${YearOfCollection} token from the Levy and Co-Invested Payments' deds cleanup scripts.

--- src/calculator/DeployLevyPayments/sql/dml/PeriodEnd.LevyPayments.Cleanup.Deds.DML.sql
+++ src/calculator/DeployLevyPayments/sql/dml/PeriodEnd.LevyPayments.Cleanup.Deds.DML.sql
@@ -1,6 +1,6 @@
 DELETE FROM ${DAS_PeriodEnd.FQ}.[Payments].[Payments]
     WHERE [Ukprn] IN (SELECT DISTINCT [Ukprn] FROM [LevyPayments].[Payments])
-        AND [CollectionPeriodName] IN (SELECT DISTINCT '${YearOfCollection}-' + [CollectionPeriodName] FROM [LevyPayments].[Payments])
+        AND [CollectionPeriodName] IN (SELECT DISTINCT [CollectionPeriodName] FROM [LevyPayments].[Payments])
         AND [CollectionPeriodMonth] IN (SELECT DISTINCT [CollectionPeriodMonth] FROM [LevyPayments].[Payments])
         AND [CollectionPeriodYear] IN (SELECT DISTINCT [CollectionPeriodYear] FROM [LevyPayments].[Payments])
         AND [FundingSource] = 1
