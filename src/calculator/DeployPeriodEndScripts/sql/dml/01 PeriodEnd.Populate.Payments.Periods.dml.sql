cd2b03bada378510ee05966d5df4394ba3c68814
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-11-17 15:29:17 +0000

    Update the Period End Scripts to simulate private beta periods (completion date time based on the current open collection period).

--- src/calculator/DeployPeriodEndScripts/sql/dml/01 PeriodEnd.Populate.Payments.Periods.dml.sql
+++ src/calculator/DeployPeriodEndScripts/sql/dml/01 PeriodEnd.Populate.Payments.Periods.dml.sql
@@ -4,13 +4,24 @@ INNER JOIN Reference.CollectionPeriods cp
 	ON p.PeriodName = '${YearOfCollection}-' + cp.Name
 GO
 
+DECLARE @now DATETIME = GETDATE()
+DECLARE @completionDateTime DATETIME
+
+SET @completionDateTime = (SELECT DATEADD(MONTH, 1, DATEFROMPARTS(q.CalendarYear, q.CalendarMonth, 1)) FROM Reference.CollectionPeriods q)
+SET @completionDateTime = DATEADD(DAY, DAY(@now) - 1, @completionDateTime)
+
+SET @completionDateTime = DATEADD(HOUR, DATEPART(HOUR, @now), @completionDateTime)
+SET @completionDateTime = DATEADD(MINUTE, DATEPART(MINUTE, @now), @completionDateTime)
+SET @completionDateTime = DATEADD(SECOND, DATEPART(SECOND, @now), @completionDateTime)
+SET @completionDateTime = DATEADD(MILLISECOND, DATEPART(MILLISECOND, @now), @completionDateTime)
+
 INSERT INTO ${DAS_PeriodEnd.FQ}.Payments.Periods
 SELECT 
-	'${YearOfCollection}-' + name, 
-	CalendarMonth, 
-	CalendarYear, 
-	(SELECT MAX(ReadDateTime) FROM ${DAS_Accounts.FQ}.dbo.DasAccountsAudit) AccountDataValidAt, 
-	(SELECT MAX(ReadDate) FROM ${DAS_Commitments.FQ}.dbo.EventStreamPointer) CommitmentDataValidAt, 
-	GETDATE() CompletionDateTime 
+    '${YearOfCollection}-' + name, 
+    CalendarMonth, 
+    CalendarYear, 
+    (SELECT MAX(ReadDateTime) FROM ${DAS_Accounts.FQ}.dbo.DasAccountsAudit) AccountDataValidAt, 
+    (SELECT MAX(ReadDate) FROM ${DAS_Commitments.FQ}.dbo.EventStreamPointer) CommitmentDataValidAt, 
+    @completionDateTime CompletionDateTime 
 FROM Reference.CollectionPeriods
 GO
