0026dad4325443c950e01b4e43d13061f8b75cd9
Simon Bull <simonbu11@users.noreply.github.com>
2017-06-21 16:46:42 +0100

    Support proportionally refunding cofunding

--- src/calculator/SFA.DAS.Payments.Calc.CoInvestedPayments.IntegrationTests/FinishOnTime/WhenRefundPaymentsAreDue.cs
+++ src/calculator/SFA.DAS.Payments.Calc.CoInvestedPayments.IntegrationTests/FinishOnTime/WhenRefundPaymentsAreDue.cs
@@ -30,7 +30,7 @@ namespace SFA.DAS.Payments.Calc.CoInvestedPayments.IntegrationTests.FinishOnTime
             _uln = Random.Next(1, int.MaxValue);
 
 
-            TestDataHelper.AddCommitment(_commitmentId, accountId, ukprn: _ukprn,uln:_uln, isDeds: false);
+            TestDataHelper.AddCommitment(_commitmentId, accountId, ukprn: _ukprn, uln: _uln, isDeds: false);
             TestDataHelper.AddCommitment(_commitmentId, accountId, ukprn: _ukprn, uln: _uln, isDeds: true);
 
             TestDataHelper.AddProvider(_ukprn);
@@ -42,7 +42,7 @@ namespace SFA.DAS.Payments.Calc.CoInvestedPayments.IntegrationTests.FinishOnTime
 
 
         [Test]
-        public void ThenCoInvestedRefundPaymentsAreMade()
+        public void ThenCoInvestedRefundPaymentsAreMadeWhenFullyRefunding()
         {
             // Arrange
             var requiredPaymentId = Guid.NewGuid().ToString();
@@ -61,20 +61,49 @@ namespace SFA.DAS.Payments.Calc.CoInvestedPayments.IntegrationTests.FinishOnTime
 
             TestDataHelper.AddPaymentDueForProvider(_commitmentId, _ukprn, amountDue: -1500, sfaContributionPercentage: 0.9m);
 
+            // Act
+            _uut.Execute(_taskContext);
+
+            //Assert
+            var payments = TestDataHelper.GetPaymentsForCommitment(_commitmentId);
+            Assert.IsTrue(payments.Count() == 2);
+
+            Assert.IsNotNull(payments.SingleOrDefault(p => p.FundingSource == (int)FundingSource.CoInvestedSfa && p.Amount == -1350));
+            Assert.IsNotNull(payments.SingleOrDefault(p => p.FundingSource == (int)FundingSource.CoInvestedEmployer && p.Amount == -150));
+        }
+
+
+
+        [Test]
+        public void ThenCoInvestedRefundPaymentsAreMadeWhenPartiallyRefunding()
+        {
+            // Arrange
+            var requiredPaymentId = Guid.NewGuid().ToString();
+
+            TestDataHelper.AddPaymentDueForProvider2(
+                    _commitmentId,
+                    _ukprn, 8, 2016,
+                    amountDue: 1500,
+                    transactionType: TransactionType.Learning,
+                    requiredPaymentId: requiredPaymentId,
+                    isDeds: true);
+
+            TestDataHelper.AddPaymentHistoryForCommitment(requiredPaymentId, FundingSource.CoInvestedEmployer, 150, 8, 2016, TransactionType.Learning, true);
+            TestDataHelper.AddPaymentHistoryForCommitment(requiredPaymentId, FundingSource.CoInvestedSfa, 1350, 8, 2016, TransactionType.Learning, true);
+            TestDataHelper.PopulatePaymentsHistory();
+
+            TestDataHelper.AddPaymentDueForProvider(_commitmentId, _ukprn, amountDue: -750, sfaContributionPercentage: 0.9m);
 
             // Act
             _uut.Execute(_taskContext);
 
             //Assert
-          
-
             var payments = TestDataHelper.GetPaymentsForCommitment(_commitmentId);
             Assert.IsTrue(payments.Count() == 2);
 
-            Assert.IsNotNull(
-                payments.SingleOrDefault(p => p.FundingSource == (int)FundingSource.CoInvestedSfa && p.Amount == -1350));
-            Assert.IsNotNull(
-                payments.SingleOrDefault(p => p.FundingSource == (int)FundingSource.CoInvestedEmployer && p.Amount == -150));
+            var actualPaymentsMessage = "actually paid:\n" + payments.Select(x => x.Amount.ToString() + " " + ((FundingSource)x.FundingSource).ToString()).Aggregate((x, y) => $"{x}\n{y}");
+            Assert.IsNotNull(payments.SingleOrDefault(p => p.FundingSource == (int)FundingSource.CoInvestedSfa && p.Amount == -675), $"Expected -675 CoInvestedSfa, {actualPaymentsMessage}");
+            Assert.IsNotNull(payments.SingleOrDefault(p => p.FundingSource == (int)FundingSource.CoInvestedEmployer && p.Amount == -75), $"Expected -75 CoInvestedEmployer, {actualPaymentsMessage}");
         }
     }
 }
