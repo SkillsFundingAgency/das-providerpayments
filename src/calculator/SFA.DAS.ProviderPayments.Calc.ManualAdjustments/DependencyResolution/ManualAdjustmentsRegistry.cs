547bfe840695e51b5a38c69c9c2af81263e66a45
Simon Bull <simonbu11@users.noreply.github.com>
2017-07-17 10:27:41 +0100

    Add manual adjustments skeleton

---
+++ src/calculator/SFA.DAS.ProviderPayments.Calc.ManualAdjustments/DependencyResolution/ManualAdjustmentsRegistry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using MediatR;
+using NLog;
+using SFA.DAS.Payments.DCFS.Context;
+using StructureMap;
+
+namespace SFA.DAS.ProviderPayments.Calc.ManualAdjustments.DependencyResolution
+{
+    public class ManualAdjustmentsRegistry : Registry
+    {
+        public ManualAdjustmentsRegistry(Type taskType, ContextWrapper contextWrapper)
+        {
+            Scan(
+               scan =>
+               {
+                   scan.AssemblyContainingType<ManualAdjustmentsRegistry>();
+
+                   scan.RegisterConcreteTypesAgainstTheFirstInterface();
+               });
+
+            For<ContextWrapper>().Use(contextWrapper);
+
+            For<ILogger>().Use(() => LogManager.GetLogger(taskType.FullName));
+
+            For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => GetInstance(ctx, t));
+            For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => GetAllInstances(ctx, t));
+            For<IMediator>().Use<Mediator>();
+        }
+
+        private static IEnumerable<object> GetAllInstances(IContext ctx, Type t)
+        {
+            return ctx.GetAllInstances(t);
+        }
+
+        private static object GetInstance(IContext ctx, Type t)
+        {
+            return ctx.GetInstance(t);
+        }
+    }
+}
---
+++ src/calculator/SFA.DAS.ProviderPayments.Calc.ManualAdjustments/ManualAdjustmentsTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SFA.DAS.Payments.DCFS;
+using SFA.DAS.Payments.DCFS.Context;
+using SFA.DAS.Payments.DCFS.Infrastructure.DependencyResolution;
+using SFA.DAS.ProviderPayments.Calc.ManualAdjustments.DependencyResolution;
+
+namespace SFA.DAS.ProviderPayments.Calc.ManualAdjustments
+{
+    public class ManualAdjustmentsTask : DcfsTask
+    {
+        private const string DatabaseSchema = "ProviderAdjustments";
+
+        private readonly IDependencyResolver _dependencyResolver;
+
+        public ManualAdjustmentsTask() 
+            : base(DatabaseSchema)
+        {
+            _dependencyResolver = new TaskDependencyResolver();
+        }
+        internal ManualAdjustmentsTask(IDependencyResolver dependencyResolver)
+            : base(DatabaseSchema)
+        {
+            _dependencyResolver = dependencyResolver;
+        }
+
+        protected override void Execute(ContextWrapper context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
