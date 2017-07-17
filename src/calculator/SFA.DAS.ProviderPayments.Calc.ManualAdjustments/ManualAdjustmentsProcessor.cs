e12418468c18b0d735fd231e76582632a43a2c66
Simon Bull <simonbu11@users.noreply.github.com>
2017-07-17 10:35:45 +0100

    Implement manual adjustments task

---
+++ src/calculator/SFA.DAS.ProviderPayments.Calc.ManualAdjustments/ManualAdjustmentsProcessor.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.ProviderPayments.Calc.ManualAdjustments
+{
+    public class ManualAdjustmentsProcessor
+    {
+
+        public virtual void Process()
+        {
+
+        }
+    }
+}
--- src/calculator/SFA.DAS.ProviderPayments.Calc.ManualAdjustments/ManualAdjustmentsTask.cs
+++ src/calculator/SFA.DAS.ProviderPayments.Calc.ManualAdjustments/ManualAdjustmentsTask.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using SFA.DAS.Payments.DCFS;
+﻿using SFA.DAS.Payments.DCFS;
 using SFA.DAS.Payments.DCFS.Context;
 using SFA.DAS.Payments.DCFS.Infrastructure.DependencyResolution;
+using SFA.DAS.ProviderPayments.Calc.Common.Context;
 using SFA.DAS.ProviderPayments.Calc.ManualAdjustments.DependencyResolution;
 
 namespace SFA.DAS.ProviderPayments.Calc.ManualAdjustments
@@ -25,7 +25,38 @@ namespace SFA.DAS.ProviderPayments.Calc.ManualAdjustments
 
         protected override void Execute(ContextWrapper context)
         {
-            throw new NotImplementedException();
+            _dependencyResolver.Init(typeof(ManualAdjustmentsTask), context);
+
+            var processor = _dependencyResolver.GetInstance<ManualAdjustmentsProcessor>();
+
+            processor.Process();
+        }
+
+        protected override bool IsValidContext(ContextWrapper contextWrapper)
+        {
+            if (string.IsNullOrEmpty(contextWrapper.GetPropertyValue(PaymentsContextPropertyKeys.YearOfCollection)))
+            {
+                throw new PaymentsInvalidContextException(PaymentsInvalidContextException.ContextPropertiesNoYearOfCollectionMessage);
+            }
+
+            return IsValidYearOfCollection(contextWrapper.GetPropertyValue(PaymentsContextPropertyKeys.YearOfCollection))
+                   && base.IsValidContext(contextWrapper);
+        }
+
+        private bool IsValidYearOfCollection(string yearOfCollection)
+        {
+            int year1;
+            int year2;
+
+            if (yearOfCollection.Length != 4 ||
+                !int.TryParse(yearOfCollection.Substring(0, 2), out year1) ||
+                !int.TryParse(yearOfCollection.Substring(2, 2), out year2) ||
+                (year2 != year1 + 1))
+            {
+                throw new PaymentsInvalidContextException(PaymentsInvalidContextException.ContextPropertiesInvalidYearOfCollectionMessage);
+            }
+
+            return true;
         }
     }
 }
