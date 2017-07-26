d36f3daa60a89ee0b91da9207000f58d4f512281
msbaig <ms_baig@hotmail.com>
2017-07-26 11:03:05 +0100

    fixed db name

--- src/calculator/DeployPaymentsDue/sql/MigrationScripts/3_PeriodEnd.Deds.PaymentsDue_Update_data_aimRef_startdate.sql
+++ src/calculator/DeployPaymentsDue/sql/MigrationScripts/3_PeriodEnd.Deds.PaymentsDue_Update_data_aimRef_startdate.sql
@@ -1,29 +1,39 @@
- Update rp
- Set rp.LearnAimRef = IsNull(ld.LearnAimRef,'ZPROG001'),
- rp.LearningStartDate = IsNull(ld.LearnStartDate,'2017-05-01') 
+Update rp
+Set rp.LearnAimRef = IsNull(ld.LearnAimRef,'ZPROG001'),
+rp.LearningStartDate = IsNull(ld.LearnStartDate,'2017-05-01') 
 from PaymentsDue.RequiredPayments rp 
-LEFT JOIN ${DS_SILR1617_Collection.databasename}.valid.LearningDelivery ld
+LEFT JOIN ${ILR_Deds.FQ}.Valid.LearningDelivery ld
 on rp.Ukprn = ld.UKPRN
-	and rp.LearnRefNumber = ld.LearnRefNumber
-	and rp.StandardCode = ld.StdCode
-	and rp.FrameworkCode = ld.FworkCode
-	and rp.PathwayCode = ld.PwayCode
-	and rp.ProgrammeType = ld.ProgType
+       and rp.LearnRefNumber = ld.LearnRefNumber
+       and rp.LearnRefNumber = ld.LearnRefNumber
+       and IsNull(rp.StandardCode,0) = IsNull(ld.StdCode,0)
+       and IsNull(rp.FrameworkCode,0) = IsNull(ld.FworkCode,0)
+       and IsNull(rp.PathwayCode ,0)= IsNull(ld.PwayCode,0)
+       and (IsNull(rp.ProgrammeType,0) = IsNull(ld.ProgType,0) OR  IsNull(rp.StandardCode,0) > 0)
 Where ld.LearnAimRef = 'ZPROG001' and ld.FundModel=36
 and TransactionType NOT IN (13,14)
 
 
- Update rp
- Set rp.LearnAimRef = IsNull(ld.LearnAimRef,''),
- rp.LearningStartDate = IsNull(ld.LearnStartDate,'2017-05-01') 
+Update rp
+Set rp.LearnAimRef = IsNull(ld.LearnAimRef,''),
+rp.LearningStartDate = IsNull(ld.LearnStartDate,'2017-05-01') 
  
 from PaymentsDue.RequiredPayments rp 
-LEFT JOIN ${DS_SILR1617_Collection.databasename}.valid.LearningDelivery ld
+LEFT JOIN ${ILR_Deds.FQ}.Valid.LearningDelivery ld
+
 on rp.Ukprn = ld.UKPRN
-	and rp.LearnRefNumber = ld.LearnRefNumber
-	and rp.StandardCode = ld.StdCode
-	and rp.FrameworkCode = ld.FworkCode
-	and rp.PathwayCode = ld.PwayCode
-	and rp.ProgrammeType = ld.ProgType
+       and rp.LearnRefNumber = ld.LearnRefNumber
+       and rp.LearnRefNumber = ld.LearnRefNumber
+and IsNull(rp.StandardCode,0) = IsNull(ld.StdCode,0)
+       and IsNull(rp.FrameworkCode,0) = IsNull(ld.FworkCode,0)
+       and IsNull(rp.PathwayCode ,0)= IsNull(ld.PwayCode,0)
+       and (IsNull(rp.ProgrammeType,0) = IsNull(ld.ProgType,0) OR  IsNull(rp.StandardCode,0) > 0)
 Where ld.LearnAimRef != 'ZPROG001' and ld.FundModel=36
-	and TransactionType IN (13,14)
+       and TransactionType IN (13,14)
+
+
+
+Update PaymentsDue.RequiredPayments 
+Set rp.LearnAimRef = '',
+rp.LearningStartDate = '2017-05-01'
+Where rp.LearnAimRef is null and  rp.LearningStartDate is null
