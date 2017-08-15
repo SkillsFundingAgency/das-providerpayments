6421e086a391f52b75be6c91aafa581503075c6d
Simon Bull <simonbu11@users.noreply.github.com>
2017-08-15 09:22:04 +0100

    Change DDL back for deds

--- src/calculator/DeployPaymentsDue/sql/ddl/PeriodEnd.Deds.PaymentsDue.DDL.tables.sql
+++ src/calculator/DeployPaymentsDue/sql/ddl/PeriodEnd.Deds.PaymentsDue.DDL.tables.sql
@@ -39,6 +39,6 @@ CREATE TABLE PaymentsDue.RequiredPayments
 	TransactionType int,
 	AmountDue decimal(15,5),
 	SfaContributionPercentage decimal(15,5),
-	FundingLineType varchar(100),
+	FundingLineType varchar(60),
 	UseLevyBalance bit
 )
