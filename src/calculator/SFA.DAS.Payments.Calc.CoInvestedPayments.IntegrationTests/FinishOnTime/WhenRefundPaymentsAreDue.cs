56d7e1df912984c302d33b5c662c25a3f7d042ff
Salman Baig <salman.sfa@outlook.com>
2017-09-06 10:56:40 +0100

    removed merged test which was added twice

--- src/calculator/SFA.DAS.Payments.Calc.CoInvestedPayments.IntegrationTests/FinishOnTime/WhenRefundPaymentsAreDue.cs
+++ src/calculator/SFA.DAS.Payments.Calc.CoInvestedPayments.IntegrationTests/FinishOnTime/WhenRefundPaymentsAreDue.cs
@@ -107,36 +107,7 @@ namespace SFA.DAS.Payments.Calc.CoInvestedPayments.IntegrationTests.FinishOnTime
         }
 
 
-        [Test]
-        public void ThenCoInvestedRefundPaymentsAreMadeWhenFullyRefunding()
-        {
-            // Arrange
-            var requiredPaymentId = Guid.NewGuid().ToString();
-
-            TestDataHelper.AddPaymentDueForProvider2(
-                    _commitmentId,
-                    _ukprn, 8, 2016,
-                    amountDue: 1500,
-                    transactionType: TransactionType.Learning,
-                    requiredPaymentId: requiredPaymentId,
-                    isDeds: true);
-
-            TestDataHelper.AddPaymentHistoryForCommitment(requiredPaymentId, FundingSource.CoInvestedEmployer, 150, 8, 2016, TransactionType.Learning, true);
-            TestDataHelper.AddPaymentHistoryForCommitment(requiredPaymentId, FundingSource.CoInvestedSfa, 1350, 8, 2016, TransactionType.Learning, true);
-            TestDataHelper.PopulatePaymentsHistory();
-
-            TestDataHelper.AddPaymentDueForProvider(_commitmentId, _ukprn, amountDue: -1500, sfaContributionPercentage: 0.9m);
-
-            // Act
-            _uut.Execute(_taskContext);
-
-            //Assert
-            var payments = TestDataHelper.GetPaymentsForCommitment(_commitmentId);
-            Assert.IsTrue(payments.Count() == 2);
-
-            Assert.IsNotNull(payments.SingleOrDefault(p => p.FundingSource == (int)FundingSource.CoInvestedSfa && p.Amount == -1350));
-            Assert.IsNotNull(payments.SingleOrDefault(p => p.FundingSource == (int)FundingSource.CoInvestedEmployer && p.Amount == -150));
-        }
+       
 
 
     }
