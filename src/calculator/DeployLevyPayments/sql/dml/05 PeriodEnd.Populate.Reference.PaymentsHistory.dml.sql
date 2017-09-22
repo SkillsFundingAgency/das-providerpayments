297395fda842e5bb00867a583c46cd38d86a1ab4
Salman Baig <salman.sfa@outlook.com>
2017-09-22 12:01:59 +0100

    fixed script

--- src/calculator/DeployLevyPayments/sql/dml/05 PeriodEnd.Populate.Reference.PaymentsHistory.dml.sql
+++ src/calculator/DeployLevyPayments/sql/dml/05 PeriodEnd.Populate.Reference.PaymentsHistory.dml.sql
@@ -15,6 +15,6 @@ SELECT
     FROM  ${DAS_PeriodEnd.FQ}.Payments.Payments p 
 	JOIN  ${DAS_PeriodEnd.FQ}.PaymentsDue.RequiredPayments rp on p.RequiredPaymentId = rp.Id
     WHERE rp.Ukprn IN (SELECT DISTINCT [Ukprn] FROM [Reference].[Providers])
-	AND IsNull(rp.CommitmentId) > 0
+	AND IsNull(rp.CommitmentId,0) > 0
 
 GO
