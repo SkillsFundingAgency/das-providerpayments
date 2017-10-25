9c603aa0fcb624c8401a649d6defc6e1131cf2ad
Carl Berisford-Murray <carl@cabm.co.uk>
2017-10-25 09:32:06 +0100

    Commit all files

---
+++ src/AcceptanceTesting/IlrSubmissionPortal/SFA.DAS.Payments.Automation.Application.UnitTests/GherkinSpecs/ValidateSpecsQuery/ValidateSpecsQueryRequestHandlerTests/WhenHandlingASpecWithAnInvalidCourseDuration.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using SFA.DAS.Payments.Automation.Application.GherkinSpecs;
+using SFA.DAS.Payments.Automation.Application.GherkinSpecs.ValidateSpecificationsQuery;
+
+namespace SFA.DAS.Payments.Automation.Application.UnitTests.GherkinSpecs.ValidateSpecificationsQuery.ValidateSpecificationsQueryRequestHandlerTests
+{
+    public class WhenHandlingASpecificationWithAnInvalidCourseDuration
+    {
+        private ValidateSpecificationsQueryRequestHandler _handler;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _handler = new ValidateSpecificationsQueryRequestHandler();
+        }
+
+        [Test]
+        public void ThenItShouldReturnInvalidResponseIfStandardHasDurationLessThan372Days()
+        {
+            // Arrange
+            var spec = TestData.GetValidSpecification(true, false);
+            foreach (var learner in spec.Arrangement.LearnerRecords)
+            {
+                learner.PlannedEndDate = learner.StartDate.AddDays(371);
+            }
+
+            // Act
+            var actual = _handler.Handle(new ValidateSpecificationsQueryRequest { Specifications = new[] { spec } });
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsFalse(actual.IsValid);
+            Assert.IsTrue(actual.Errors.Any(e => e.RuleId == ValidationRuleIds.StandardMinDuration));
+        }
+
+        [Test]
+        public void ThenItShouldReturnInvalidResponseIfFrameworkHasDurationLessThan365Days()
+        {
+            // Arrange
+            var spec = TestData.GetValidSpecification(false, false);
+            foreach (var learner in spec.Arrangement.LearnerRecords)
+            {
+                learner.PlannedEndDate = learner.StartDate.AddDays(364);
+            }
+
+            // Act
+            var actual = _handler.Handle(new ValidateSpecificationsQueryRequest { Specifications = new[] { spec } });
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsFalse(actual.IsValid);
+            Assert.IsTrue(actual.Errors.Any(e => e.RuleId == ValidationRuleIds.FrameworkMinDuration));
+        }
+    }
+}
---
+++ src/AcceptanceTesting/IlrSubmissionPortal/SFA.DAS.Payments.Automation.Application.UnitTests/GherkinSpecs/ValidateSpecsQuery/ValidateSpecsQueryRequestHandlerTests/WhenHandlingASpecWithAnInvalidPriceEffectiveDate.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using SFA.DAS.Payments.Automation.Application.GherkinSpecs;
+using SFA.DAS.Payments.Automation.Application.GherkinSpecs.ValidateSpecificationsQuery;
+
+namespace SFA.DAS.Payments.Automation.Application.UnitTests.GherkinSpecs.ValidateSpecificationsQuery.ValidateSpecificationsQueryRequestHandlerTests
+{
+    public class WhenHandlingASpecificationWithAnInvalidPriceEffectiveDate
+    {
+        private static readonly DateTime LearningStartDate = new DateTime(2017, 5, 18);
+
+        private ValidateSpecificationsQueryRequestHandler _handler;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _handler = new ValidateSpecificationsQueryRequestHandler();
+        }
+
+        [Test]
+        public void ThenItShouldReturnInvalidResponseIfTrainingPrice1EffectiveDateIsAfterStartOfLearning()
+        {
+            // Arrange
+            var spec = TestData.GetValidSpecification(false, false);
+            foreach (var learner in spec.Arrangement.LearnerRecords)
+            {
+                learner.StartDate = LearningStartDate;
+                learner.TotalTrainingPrice1EffectiveDate = LearningStartDate.AddDays(1);
+            }
+
+            // Act
+            var actual = _handler.Handle(new ValidateSpecificationsQueryRequest { Specifications = new[] { spec } });
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsFalse(actual.IsValid);
+            Assert.IsTrue(actual.Errors.Any(e => e.RuleId == ValidationRuleIds.PriceEffectiveTooLate));
+        }
+
+        [Test]
+        public void ThenItShouldReturnInvalidResponseIfAssessmentPrice1EffectiveDateIsAfterStartOfLearning()
+        {
+            // Arrange
+            var spec = TestData.GetValidSpecification(true, false);
+            foreach (var learner in spec.Arrangement.LearnerRecords)
+            {
+                learner.StartDate = LearningStartDate;
+                learner.TotalTrainingPrice1EffectiveDate = LearningStartDate;
+                learner.TotalAssessmentPrice1EffectiveDate = LearningStartDate.AddDays(1);
+            }
+
+            // Act
+            var actual = _handler.Handle(new ValidateSpecificationsQueryRequest { Specifications = new[] { spec } });
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsFalse(actual.IsValid);
+            Assert.IsTrue(actual.Errors.Any(e => e.RuleId == ValidationRuleIds.PriceEffectiveTooLate));
+        }
+
+        [Test]
+        public void ThenItShouldReturnInvalidResponseIfTrainingAndAssessmentPrice1EffectiveDateIsAfterStartOfLearning()
+        {
+            // Arrange
+            var spec = TestData.GetValidSpecification(false, false);
+            foreach (var learner in spec.Arrangement.LearnerRecords)
+            {
+                learner.StartDate = LearningStartDate;
+                learner.TotalTrainingPrice1EffectiveDate = LearningStartDate.AddDays(1);
+                learner.TotalAssessmentPrice1EffectiveDate = LearningStartDate.AddDays(1);
+            }
+
+            // Act
+            var actual = _handler.Handle(new ValidateSpecificationsQueryRequest { Specifications = new[] { spec } });
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsFalse(actual.IsValid);
+            Assert.AreEqual(2, actual.Errors.Count(e => e.RuleId == ValidationRuleIds.PriceEffectiveTooLate));
+        }
+
+
+
+
+
+
+        [Test]
+        public void ThenItShouldReturnInvalidResponseIfTrainingPrice1EffectiveDateIsBeforeStartOfLearning()
+        {
+            // Arrange
+            var spec = TestData.GetValidSpecification(false, false);
+            foreach (var learner in spec.Arrangement.LearnerRecords)
+            {
+                learner.StartDate = LearningStartDate;
+                learner.TotalTrainingPrice1EffectiveDate = LearningStartDate.AddDays(-1);
+            }
+
+            // Act
+            var actual = _handler.Handle(new ValidateSpecificationsQueryRequest { Specifications = new[] { spec } });
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsFalse(actual.IsValid);
+            Assert.IsTrue(actual.Errors.Any(e => e.RuleId == ValidationRuleIds.PriceEffectiveTooEarly));
+        }
+
+        [Test]
+        public void ThenItShouldReturnInvalidResponseIfAssessmentPrice1EffectiveDateIsBeforeStartOfLearning()
+        {
+            // Arrange
+            var spec = TestData.GetValidSpecification(true, false);
+            foreach (var learner in spec.Arrangement.LearnerRecords)
+            {
+                learner.StartDate = LearningStartDate;
+                learner.TotalTrainingPrice1EffectiveDate = LearningStartDate;
+                learner.TotalAssessmentPrice1EffectiveDate = LearningStartDate.AddDays(-1);
+            }
+
+            // Act
+            var actual = _handler.Handle(new ValidateSpecificationsQueryRequest { Specifications = new[] { spec } });
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsFalse(actual.IsValid);
+            Assert.IsTrue(actual.Errors.Any(e => e.RuleId == ValidationRuleIds.PriceEffectiveTooEarly));
+        }
+
+        [Test]
+        public void ThenItShouldReturnInvalidResponseIfTrainingAndAssessmentPrice1EffectiveDateIsBeforeStartOfLearning()
+        {
+            // Arrange
+            var spec = TestData.GetValidSpecification(false, false);
+            foreach (var learner in spec.Arrangement.LearnerRecords)
+            {
+                learner.StartDate = LearningStartDate;
+                learner.TotalTrainingPrice1EffectiveDate = LearningStartDate.AddDays(-1);
+                learner.TotalAssessmentPrice1EffectiveDate = LearningStartDate.AddDays(-1);
+            }
+
+            // Act
+            var actual = _handler.Handle(new ValidateSpecificationsQueryRequest { Specifications = new[] { spec } });
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsFalse(actual.IsValid);
+            Assert.AreEqual(2, actual.Errors.Count(e => e.RuleId == ValidationRuleIds.PriceEffectiveTooEarly));
+        }
+
+    }
+}
