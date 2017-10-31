382de2acdc99c7381f08666e538fcb3e0dec9db6
Carl Berisford-Murray <carl@cabm.co.uk>
2017-10-31 15:57:06 +0000

    UPdated build and package scripts for the period end specific components

--- src/PeriodEnd/RunPackComponent.bat
+++ src/PeriodEnd/RunPackComponent.bat
@@ -4,15 +4,18 @@ if "%2"=="" ( set "Component=LevyPayments" ) else ( set "Component=%2" )
 if exist "Deploy%Component%\component" ( rd /s /q "Deploy%Component%\component" )
 if exist "Deploy%Component%\test-results" ( rd /s /q "Deploy%Component%\test-results" )
 
+if exist "%Component%\Deploy%Component%" (
+  xcopy "%Component%\Deploy%Component%\*.*" "Deploy%Component%\" /s /e /y
+)
 if not exist "Deploy%Component%\component" ( md "Deploy%Component%\component" )
 if not exist "Deploy%Component%\test-results" ( md "Deploy%Component%\test-results" )
 
-if exist "SFA.DAS.ProviderPayments.Calc.%Component%\bin\%BuildConfig%" (
-  xcopy SFA.DAS.ProviderPayments.Calc.%Component%\bin\%BuildConfig%\*.dll Deploy%Component%\component\
+if exist "%Component%\SFA.DAS.ProviderPayments.Calc.%Component%\bin\%BuildConfig%" (
+  xcopy %Component%\SFA.DAS.ProviderPayments.Calc.%Component%\bin\%BuildConfig%\*.dll Deploy%Component%\component\
 )
 
-if exist "SFA.DAS.Payments.Calc.%Component%\bin\%BuildConfig%" (
-  xcopy SFA.DAS.Payments.Calc.%Component%\bin\%BuildConfig%\*.dll Deploy%Component%\component\
+if exist "%Component%\SFA.DAS.Payments.Calc.%Component%\bin\%BuildConfig%" (
+  xcopy %Component%\SFA.DAS.Payments.Calc.%Component%\bin\%BuildConfig%\*.dll Deploy%Component%\component\
 )
 
 xcopy TestResult*.Common.xml Deploy%Component%\test-results\
