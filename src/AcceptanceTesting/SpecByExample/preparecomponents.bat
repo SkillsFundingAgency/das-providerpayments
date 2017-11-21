cfeec350c0e3cddc56e5f0aee0702fd129548509
Carl Berisford-Murray <carl@cabm.co.uk>
2017-11-21 14:03:03 +0000

    Included new component definitions to acceptance test scripts

--- src/AcceptanceTesting/SpecByExample/preparecomponents.bat
+++ src/AcceptanceTesting/SpecByExample/preparecomponents.bat
@@ -2,9 +2,11 @@ mkdir ..\..\..\workingdir
 mkdir ..\..\..\workingdir\components
 
 xcopy ..\..\..\das-collectionearnings-opa-calculator ..\..\..\workingdir\components /E
-xcopy ..\..\..\das-collectionearnings-datalock ..\..\..\workingdir\components /E
+xcopy ..\..\..\das-collectionearnings-datalock\datalockperiodend* ..\..\..\workingdir\components /E
+xcopy ..\..\..\das-collectionearnings-datalock\datalocksubmission* ..\..\..\workingdir\components /E
 xcopy ..\..\..\das-providerpayments-calculator ..\..\..\workingdir\components /E
 
 xcopy ..\..\..\das-payment-reference-commitments ..\..\..\workingdir\components /E
 xcopy ..\..\..\das-payment-reference-accounts ..\..\..\workingdir\components /E
-xcopy ..\..\..\das-providerevents-components ..\..\..\workingdir\components /E
+xcopy ..\..\..\das-providerevents-datalockevents ..\..\..\workingdir\components /E
+xcopy ..\..\..\das-providerevents-submissionevents ..\..\..\workingdir\components /E
