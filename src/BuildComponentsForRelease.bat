82e1f2a301585055b7a1ddf9133619c55632c6a5
Carl Berisford-Murray <carl@cabm.co.uk>
2017-12-05 13:38:11 +0000

    RunBuild for all components is working from /src/

---
+++ src/BuildComponentsForRelease.bat
@@ -0,0 +1,20 @@
+rmdir /S /Q "..\artifacts\components"
+mkdir "..\artifacts\components"
+
+xcopy "OpaEarning\Build\Deploy\1718" "..\artifacts\components\EarningsCalculator\" /S /Y
+xcopy "SharedPipelineComponents\DataLock\DeployPeriodEnd" "..\artifacts\components\DataLockPeriodEnd\" /S /Y
+xcopy "SharedPipelineComponents\DataLock\Deploy" "..\artifacts\components\DataLockSubmission\" /S /Y
+xcopy "SharedPipelineComponents\DataLockEvents\DeployDataLock" "..\artifacts\components\DataLockEvents\" /S /Y
+xcopy "SharedPipelineComponents\IlrSubmissionEvents\DeploySubmission" "..\artifacts\components\SubmissionEvents\" /S /Y
+xcopy "Reference\Commitments\Deploy" "..\artifacts\components\ReferenceCommitments\" /S /Y
+xcopy "Reference\Accounts\Deploy" "..\artifacts\components\ReferenceAccounts\" /S /Y
+xcopy "PeriodEnd\DeployCoInvestedPayments" "..\artifacts\components\CoInvestedPaymentsCalculator\" /S /Y
+xcopy "PeriodEnd\DeployLevyPayments" "..\artifacts\components\LevyCalculator\" /S /Y
+xcopy "PeriodEnd\DeployManualAdjustments" "..\artifacts\components\ManualAdjustmentsCalculator\" /S /Y
+xcopy "PeriodEnd\DeployPaymentsDue" "..\artifacts\components\PaymentsDue\" /S /Y
+xcopy "PeriodEnd\DeployPeriodEndScripts" "..\artifacts\components\PeriodEndScripts\" /S /Y
+xcopy "PeriodEnd\DeployProviderAdjustments" "..\artifacts\components\ProviderAdjustmentsCalculator\" /S /Y
+
+
+
+
