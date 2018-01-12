e5f7bc7b64e6395e8ce00ec273041227411b31f6
phildavies <github@phildavies.co.uk>
2018-01-12 16:04:52 +0000

    Change order of DataLockEvent & related tables copying from transient to deds + Fix build of ProviderPayments.TestStack solution + add ignored test useful for manually testing DataLockEvent table transfers

---
+++ src/SharedPipelineComponents/DataLockEvents/SFA.DAS.Provider.Events.DataLock.IntegrationTests/Transfer/WhenDataLockEventsAreTransferred.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using CS.Common.SqlBulkCopyCat;
+using CS.Common.SqlBulkCopyCat.Model.Config.Builder;
+using SFA.DAS.Provider.Events.DataLock.IntegrationTests.Helpers;
+using SFA.DAS.Provider.Events.DataLock.IntegrationTests.TestContext;
+
+// this (commented out) test can be used to manually test the transer from transient db to Deds dbof DataLockEvents and related tables
+
+namespace SFA.DAS.Provider.Events.DataLock.IntegrationTests.Transfer
+{
+    class WhenDataLockEventsAreTransferred
+    {
+        //// comment out in GlobalSetup...
+        //[OneTimeSetUp]
+        //public void BeforeAllTests()
+        //{
+        //    //SetupDedsDatabase();
+        //    //SetupSubmissionTransientDatabase();
+        //    //SetupPeriodEndTransientDatabase();
+        //}
+
+        [Test]
+        [Ignore("Useful for manual testing only (without further work)")]
+        public void ThenItShouldTransfer()
+        {
+            //TestDataHelper.SubmissionCopyReferenceData();
+
+            const string mappingFilePath = @"C:\git\das-providerpayments\src\SharedPipelineComponents\DataLockEvents\DeployDataLock\copy mappings\DasDataLockEventsCopyToDedsMapping.xml";
+
+            var config = new CopyCatConfigBuilder().FromXmlFile(mappingFilePath);
+            config.SourceConnectionString = GlobalTestContext.Current.TransientSubmissionDatabaseConnectionString;
+            config.DestinationConnectionString = GlobalTestContext.Current.DedsDatabaseConnectionString;
+
+            var copyCat = new CopyCat(config);
+            copyCat.Copy();
+        }
+    }
+}
