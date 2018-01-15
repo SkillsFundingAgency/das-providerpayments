1b6c385fcd271746fe2e05067e1911c3a380579b
phildavies <github@phildavies.co.uk>
2018-01-15 09:17:06 +0000

    tidy up ignored WhenDataLockEventsAreTransferred unit test

--- src/SharedPipelineComponents/DataLockEvents/SFA.DAS.Provider.Events.DataLock.IntegrationTests/Transfer/WhenDataLockEventsAreTransferred.cs
+++ src/SharedPipelineComponents/DataLockEvents/SFA.DAS.Provider.Events.DataLock.IntegrationTests/Transfer/WhenDataLockEventsAreTransferred.cs
@@ -1,30 +1,23 @@
 ﻿using NUnit.Framework;
 using CS.Common.SqlBulkCopyCat;
 using CS.Common.SqlBulkCopyCat.Model.Config.Builder;
-using SFA.DAS.Provider.Events.DataLock.IntegrationTests.Helpers;
 using SFA.DAS.Provider.Events.DataLock.IntegrationTests.TestContext;
-
-// this ignored test can be used to manually test the transer from transient db to Deds db of DataLockEvents and related tables
 
 namespace SFA.DAS.Provider.Events.DataLock.IntegrationTests.Transfer
 {
     class WhenDataLockEventsAreTransferred
     {
-        //// comment out in GlobalSetup...
-        //[OneTimeSetUp]
-        //public void BeforeAllTests()
-        //{
-        //    //SetupDedsDatabase();
-        //    //SetupSubmissionTransientDatabase();
-        //    //SetupPeriodEndTransientDatabase();
-        //}
-
+        /// <summary>
+        /// This ignored test can be used to manually test the transer from transient db to Deds db of DataLockEvents and related tables.
+        /// </summary>
+        /// <remarks>
+        /// Remove BeforeAllTests() in GlobalSetup before running, to stop your test data in the transient db from being deleted.
+        /// Correct the mappingFilePath for your environment, or make it relative.
+        /// </remarks>
         [Test]
         [Ignore("Useful for manual testing only (without further work)")]
         public void ThenItShouldTransfer()
         {
-            //TestDataHelper.SubmissionCopyReferenceData();
-
             const string mappingFilePath = @"C:\git\das-providerpayments\src\SharedPipelineComponents\DataLockEvents\DeployDataLock\copy mappings\DasDataLockEventsCopyToDedsMapping.xml";
 
             var config = new CopyCatConfigBuilder().FromXmlFile(mappingFilePath);
