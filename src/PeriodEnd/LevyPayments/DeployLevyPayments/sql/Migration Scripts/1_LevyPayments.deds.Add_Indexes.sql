eaee19929497876845b8e171bd9720e182a46ed7
phildavies <github@phildavies.co.uk>
2018-01-19 11:14:33 +0000

    Add back index creation on DataLock.[DataLockEventPeriods|DataLockEventCommitmentVersions] under DataLockEvents (where the tables are created) + add 'if exists - drop' to index creation for Payments.Payments + remove deadwood from ProviderPayments.TestStack.UI.csproj

--- src/PeriodEnd/LevyPayments/DeployLevyPayments/sql/Migration Scripts/1_LevyPayments.deds.Add_Indexes.sql
+++ src/PeriodEnd/LevyPayments/DeployLevyPayments/sql/Migration Scripts/1_LevyPayments.deds.Add_Indexes.sql
@@ -1,3 +1,12 @@
+IF EXISTS (SELECT * FROM sys.indexes i
+JOIN sys.objects t ON i.object_id = t.object_id
+WHERE t.name = 'Payments'
+AND i.name = 'IX_Payments_RequiredPaymentId')
+BEGIN
+	DROP INDEX IX_Payments_RequiredPaymentId ON Payments.Payments
+END
+GO
+
 CREATE INDEX IX_Payments_RequiredPaymentId ON Payments.Payments (
 	RequiredPaymentId, 
 	CollectionPeriodName, 
@@ -5,9 +14,3 @@ CREATE INDEX IX_Payments_RequiredPaymentId ON Payments.Payments (
 	CollectionPeriodYear, 
 	FundingSource)
 GO
-
-
-
-
-
-
