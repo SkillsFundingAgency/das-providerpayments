f9a8fc3800457afe0247600efcd2cf8bfc0541a1
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-01-22 13:12:40 +0000

    Comand timeout code for datalock

--- src/SharedPipelineComponents/Datalock/SFA.DAS.CollectionEarnings.DataLock/DataLockProcessor.cs
+++ src/SharedPipelineComponents/Datalock/SFA.DAS.CollectionEarnings.DataLock/DataLockProcessor.cs
@@ -37,6 +37,7 @@ namespace SFA.DAS.CollectionEarnings.DataLock
         public virtual void Process()
         {
             _logger.Info("Started Data Lock Processor.");
+            _logger.Info($"Using timeout of: {DataLockTask.CommandTimeout}");
 
             var providersQueryResponse = ReturnValidGetProvidersQueryResponseOrThrow();
 
