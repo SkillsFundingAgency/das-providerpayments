7beea4fd96d82c634bf1a3cd7edba584e73a96a6
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-01-22 15:59:11 +0000

    Added indexes that have been added to production

--- src/PeriodEnd/PaymentsDue/DeployPaymentsDue/sql/MigrationScripts/5_PeriodEnd.Deds.PaymentsDue.Add_Indexes.sql
+++ src/PeriodEnd/PaymentsDue/DeployPaymentsDue/sql/MigrationScripts/5_PeriodEnd.Deds.PaymentsDue.Add_Indexes.sql
@@ -1,11 +1,12 @@
 
 
-CREATE INDEX IX_Payments_RequiredPaymentId ON Payments.Payments (
-	RequiredPaymentId, 
-	CollectionPeriodName, 
-	CollectionPeriodMonth, 
-	CollectionPeriodYear, 
-	FundingSource)
+IF NOT EXISTS (SELECT 1 FROM sys.indexes i
+				JOIN sys.objects t ON i.object_id = t.object_id
+				WHERE t.name = 'Payments'
+				AND i.name = 'IX_Payments_RequiredPayment')
+BEGIN
+	CREATE INDEX IX_Payments_RequiredPayment ON Payments.Payments (RequiredPaymentId, CollectionPeriodName, CollectionPeriodMonth, CollectionPeriodYear, FundingSource);
+END
 GO
 
 
@@ -13,8 +14,10 @@ GO
 --CREATE INDEX IX_RequiredPayments_Id_UKPRN_CommitmentId ON PaymentsDue.RequiredPayments (Id, UKPRN, CommitmentId)
 --GO
 
-
-CREATE INDEX IX_RequiredPayments_Ukprn ON PaymentsDue.RequiredPayments (UKPRN)
+IF NOT EXISTS (SELECT NULL FROM sys.indexes WHERE name = 'IX_RequiredPayments_Ukprn')
+BEGIN
+	CREATE INDEX IX_RequiredPayments_Ukprn ON PaymentsDue.RequiredPayments (UKPRN)
+END
 GO
 
 
