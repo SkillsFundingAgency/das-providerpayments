4d763272b30abb24d5a5bc0b392d75dd4061c7c4
tom-gough <tom.gough.tgsd@outlook.com>
2018-02-20 12:54:25 +0000

    Refund rule now asserts if a refund is made. (untested pushing to share with Dave)

--- src/AcceptanceTesting/SpecByExample/SFA.DAS.Payments.AcceptanceTests/Assertions/PaymentsAndEarningsRules/RefundTakenBySfaRule.cs
+++ src/AcceptanceTesting/SpecByExample/SFA.DAS.Payments.AcceptanceTests/Assertions/PaymentsAndEarningsRules/RefundTakenBySfaRule.cs
@@ -1,13 +1,30 @@
 using System.Collections.Generic;
 using System.Linq;
-
+using SFA.DAS.Payments.AcceptanceTests.Contexts;
 using SFA.DAS.Payments.AcceptanceTests.ReferenceDataModels;
+using SFA.DAS.Payments.AcceptanceTests.ResultsDataModels;
 
 namespace SFA.DAS.Payments.AcceptanceTests.Assertions.PaymentsAndEarningsRules
 {
-    public class RefundTakenBySfaRule : ProviderPaidBySfaRule
+    public class RefundTakenBySfaRule : PaymentsRuleBase
     {
-        
+        public override void AssertBreakdown(EarningsAndPaymentsBreakdown breakdown, IEnumerable<LearnerResults> submissionResults, EmployerAccountContext employerAccountContext)
+        {
+            var allPayments = GetPaymentsForBreakdown(breakdown, submissionResults)
+                .Where(p => p.FundingSource != FundingSource.CoInvestedEmployer && p.Amount < 0)
+                .ToArray();
+            foreach (var period in breakdown.RefundTakenBySfa)
+            {
+                var prevPeriod = new PeriodValue
+                {
+                    PeriodName = period.PeriodName.ToPeriodDateTime().AddMonths(-1).ToPeriodName(),
+                    Value = period.Value
+                };
+
+                AssertResultsForPeriod(prevPeriod, allPayments);
+            }
+        }
+
         protected override string FormatAssertionFailureMessage(PeriodValue period, decimal actualPaymentInPeriod)
         {
             var specPeriod = period.PeriodName.ToPeriodDateTime().AddMonths(1).ToPeriodName();
