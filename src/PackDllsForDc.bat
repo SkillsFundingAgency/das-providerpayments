cef7186bdb3abfe8e064c5d538cce16fbf319038
Maxim Beliy <maxim.beliy@mobiess.com>
2018-03-02 11:26:41 +0000

    artifacts matching DAS.PeriodEnd project structure

--- src/PackDllsForDc.bat
+++ src/PackDllsForDc.bat
@@ -1,16 +1,16 @@
 if "%1"=="" ( set "BuildConfig=Debug" ) else ( set "BuildConfig=%1" )
 
-if exist DcArtifacts RmDir DcArtifacts /s /q
-mkdir "DcArtifacts" 
-mkdir "DcArtifacts/PeriodEnd" 
-mkdir "DcArtifacts/IlrSubmission" 
-mkdir "DcArtifacts/Accounts/Build" 
-mkdir "DcArtifacts/Commitments" 
+rmdir "DcArtifacts/bin"  /s /q
+mkdir "DcArtifacts/bin" 
+mkdir "DcArtifacts/bin/PeriodEnd" 
+mkdir "DcArtifacts/bin/IlrSubmission" 
+mkdir "DcArtifacts/bin/Accounts/Build" 
+mkdir "DcArtifacts/bin/Commitments" 
 
 
-xcopy "Reference\Accounts\SFA.DAS.Payments.Reference.Accounts\bin\%BuildConfig%\*.dll" "DcArtifacts/Accounts/Build" /s /e /y 
+xcopy "Reference\Accounts\SFA.DAS.Payments.Reference.Accounts\bin\%BuildConfig%\*.dll" "DcArtifacts/bin/Accounts/Build" /s /e /y 
 
-..\Tools\IlMerge\IlMerge /lib:DcArtifacts\Accounts\Build /wildcards /closed /out:DcArtifacts\Accounts\DasReference.BinaryTask.dll /targetplatform:v4 *.dll
+..\Tools\IlMerge\IlMerge /lib:DcArtifacts\bin\Accounts\Build /wildcards /closed /out:DcArtifacts\bin\Accounts\DasReference.BinaryTask.dll /targetplatform:v4 *.dll
 
 
 
