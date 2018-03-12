8f7c66918be747d773afd439e1f57b8e52763b75
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-03-12 15:42:37 +0000

    Can't make tests fail for programme type = null from transaction type 13,14,15. Will do via AT

--- src/PeriodEnd/PaymentsDue/DeployPaymentsDue/sql/dml/13 PeriodEnd.PaymentsDue.PreRun.Staging.ApprenticeshipEarnings2.sql
+++ src/PeriodEnd/PaymentsDue/DeployPaymentsDue/sql/dml/13 PeriodEnd.PaymentsDue.PreRun.Staging.ApprenticeshipEarnings2.sql
@@ -44,12 +44,13 @@ SELECT
 
         FROM Reference.ApprenticeshipEarnings ae
 			JOIN  Staging.LearnerPriceEpisodePerPeriod pae
-			ON ae.Ukprn = pae.Ukprn
-			AND ae.LearnRefNumber = pae.LearnRefNumber
-			AND ae.AimSeqNumber = pae.AimSeqNumber
-			AND ae.Period = pae.Period
-			AND ae.EpisodeStartDate = pae.MaxEpisodeStartDate
-            JOIN Reference.ApprenticeshipDeliveryEarnings ade ON ae.Ukprn = ade.Ukprn
+				ON ae.Ukprn = pae.Ukprn
+				AND ae.LearnRefNumber = pae.LearnRefNumber
+				AND ae.AimSeqNumber = pae.AimSeqNumber
+				AND ae.Period = pae.Period
+				AND ae.EpisodeStartDate = pae.MaxEpisodeStartDate
+            JOIN Reference.ApprenticeshipDeliveryEarnings ade 
+				ON ae.Ukprn = ade.Ukprn
                 AND ae.LearnRefNumber = ade.LearnRefNumber
                 AND ISNULL(ae.StandardCode, -1) = ISNULL(ade.StandardCode, -1)
                 AND ISNULL(ae.FrameworkCode, -1) = ISNULL(ade.FrameworkCode, -1)
@@ -58,31 +59,35 @@ SELECT
                 AND ae.Period = ade.Period
 			JOIN Staging.CollectionPeriods cp
               ON ade.Period = cp.PeriodNumber
-            LEFT JOIN DataLock.PriceEpisodeMatch pem ON ae.Ukprn = pem.Ukprn
+            LEFT JOIN DataLock.PriceEpisodeMatch pem 
+				ON ae.Ukprn = pem.Ukprn
                 AND ae.PriceEpisodeIdentifier = pem.PriceEpisodeIdentifier
                 AND ae.LearnRefNumber = pem.LearnRefNumber
                 AND ae.AimSeqNumber = pem.AimSeqNumber
-            LEFT JOIN DataLock.PriceEpisodePeriodMatch pepm ON ae.Ukprn = pepm.Ukprn
+            LEFT JOIN DataLock.PriceEpisodePeriodMatch pepm 
+				ON ae.Ukprn = pepm.Ukprn
                 AND ae.PriceEpisodeIdentifier = pepm.PriceEpisodeIdentifier
                 AND ae.LearnRefNumber = pepm.LearnRefNumber
                 AND ae.AimSeqNumber = pepm.AimSeqNumber
                 AND ae.Period = pepm.Period
-            LEFT JOIN Reference.DasCommitments c ON c.CommitmentId = pepm.CommitmentId
+            LEFT JOIN Reference.DasCommitments c 
+				ON c.CommitmentId = pepm.CommitmentId
                 AND c.VersionId = pepm.VersionId
-            LEFT JOIN Reference.DasAccounts a ON c.AccountId = a.AccountId
-			LEFT JOIN Staging.NonDasTransactionTypes ndtt ON ndtt.ApprenticeshipContractType = ae.ApprenticeshipContractType
-
+            LEFT JOIN Reference.DasAccounts a 
+				ON c.AccountId = a.AccountId
+			LEFT JOIN Staging.NonDasTransactionTypes ndtt 
+				ON ndtt.ApprenticeshipContractType = ae.ApprenticeshipContractType
 			LEFT JOIN Reference.RequiredPaymentsHistory ph 
-		ON	ae.Ukprn = ph.Ukprn
-              AND ae.LearnrefNumber = ph.LearnrefNumber
-              AND ae.StandardCode = ph.StandardCode
-              AND ISNULL(ae.ProgrammeType,0) = ISNULL(ph.ProgrammeType,0)
-              AND ISNULL(ae.FrameworkCode,0) = ISNULL(ph.FrameworkCode,0)
-              AND ISNULL(ae.PathwayCode,0) = ISNULL(ph.PathwayCode,0)
-              AND ndtt.TransactionType = ph.TransactionType
-			  AND ndtt.ApprenticeshipContractType = ph.ApprenticeshipContractType
-              AND cp.CalendarMonth = ph.DeliveryMonth
-              AND cp.CalendarYear = ph.DeliveryYear
+				ON	ae.Ukprn = ph.Ukprn
+				AND ae.LearnrefNumber = ph.LearnrefNumber
+				AND ae.StandardCode = ph.StandardCode
+				AND ISNULL(ae.ProgrammeType,0) = ISNULL(ph.ProgrammeType,0)
+				AND ISNULL(ae.FrameworkCode,0) = ISNULL(ph.FrameworkCode,0)
+				AND ISNULL(ae.PathwayCode,0) = ISNULL(ph.PathwayCode,0)
+				AND ndtt.TransactionType = ph.TransactionType
+				AND ndtt.ApprenticeshipContractType = ph.ApprenticeshipContractType
+				AND cp.CalendarMonth = ph.DeliveryMonth
+				AND cp.CalendarYear = ph.DeliveryYear
 	WHERE 
 	(
 	(COALESCE(pepm.TransactionTypesFlag, 1) = 1  And ndtt.TransactionType = 13  AND ade.MathEngOnProgPayment <> 0 ) OR
