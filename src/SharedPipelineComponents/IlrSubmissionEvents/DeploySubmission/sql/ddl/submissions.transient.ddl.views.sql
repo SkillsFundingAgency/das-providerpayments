8583d29c33edc928356e3cb9a532c1c06bb71d4c
David Gouge <davegouge@gmail.com>
2018-04-03 09:18:52 +0100

    Added FamilyName GivenNames to LastSeenVersion

--- src/SharedPipelineComponents/IlrSubmissionEvents/DeploySubmission/sql/ddl/submissions.transient.ddl.views.sql
+++ src/SharedPipelineComponents/IlrSubmissionEvents/DeploySubmission/sql/ddl/submissions.transient.ddl.views.sql
@@ -46,7 +46,9 @@ SELECT
 	ld.LearnStartDate,
 	ld.LearnPlanEndDate,
 	ld.LearnActEndDate,
-	ld.EPAOrgID
+	ld.EPAOrgID,
+	l.FamilyName,
+	l.GivenNames
 FROM Valid.LearningDelivery ld
 INNER JOIN Valid.Learner l
 	ON ld.LearnRefNumber = l.LearnRefNumber
@@ -130,7 +132,9 @@ SELECT
 	ld.NINumber,
 	pe.CommitmentId,
 	pe.EmpId As EmployerReferenceNumber,
-	ld.EPAOrgId
+	ld.EPAOrgId,
+	ld.FamilyName,
+	ld.GivenNames
 FROM Submissions.vw_Providers p
 INNER JOIN Submissions.vw_LearningDeliveries ld
 	ON p.UKPRN = ld.UKPRN
