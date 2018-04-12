ce6af81e032904bbf5c26e352e07fe70d4318df2
Michael Young <mick.young1981@googlemail.com>
2018-04-12 10:31:41 +0100

    DPP-1289 Added indexes required for the provider events API (#181)
    DPP-1289 Added indexes required for the provider events API

--- src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue.IntegrationTests/GlobalSetup.cs
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue.IntegrationTests/GlobalSetup.cs
@@ -44,6 +44,7 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.IntegrationTests
                     RunSqlScript(@"1_PeriodEnd.Deds.PaymentsDue.Change_CommitmentVersionId_Type.sql", dedsConnection);
                     RunSqlScript(@"2_PeriodEnd.Deds.PaymentsDue.Add_Columns.sql", dedsConnection);
                     RunSqlScript(@"4_PeriodEnd.Deds.PaymentsDue_FundingLienType_varchar100.sql", dedsConnection);
+                    RunSqlScript(@"6_PeriodEnd.Deds.PaymentsDue.Add_APIIndexes.sql", dedsConnection);
 
                     RunSqlScript(@"PeriodEnd.Transient.PaymentsDue.Reference.DDL.tables.sql", transientConnection);
                     RunSqlScript(@"PeriodEnd.Transient.Reference.CollectionPeriods.ddl.tables.sql", transientConnection);
