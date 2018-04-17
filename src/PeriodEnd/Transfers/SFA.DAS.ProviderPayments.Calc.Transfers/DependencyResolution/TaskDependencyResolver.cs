9fa16267688fbd46cff92dfe726c7314106f44f1
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-04-17 12:00:04 +0100

    Added skeleton DCFS structure to transfers component

---
+++ src/PeriodEnd/Transfers/SFA.DAS.ProviderPayments.Calc.Transfers/DependencyResolution/TaskDependencyResolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SFA.DAS.Payments.DCFS.StructureMap.Infrastructure.DependencyResolution;
+using StructureMap;
+using SFA.DAS.Payments.DCFS.Context;
+
+namespace SFA.DAS.ProviderPayments.Calc.Transfers.DependencyResolution
+{
+    public class TaskDependencyResolver : StructureMapDependencyResolver
+    {
+        protected override Registry CreateRegistry(Type taskType, ContextWrapper contextWrapper)
+        {
+            return new TransfersRegistry(taskType, contextWrapper);
+        }
+    }
+}
