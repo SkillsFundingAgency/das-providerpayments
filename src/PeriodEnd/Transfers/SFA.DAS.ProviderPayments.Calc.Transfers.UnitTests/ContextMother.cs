b001a312c05bb62a7110c97277562fe17caae1ec
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-04-18 10:13:02 +0100

    Added unit test foundations

---
+++ src/PeriodEnd/Transfers/SFA.DAS.ProviderPayments.Calc.Transfers.UnitTests/ContextMother.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using CS.Common.External.Interfaces;
+using Moq;
+using SFA.DAS.Payments.DCFS.Context;
+
+namespace SFA.DAS.ProviderPayments.Calc.Transfers.UnitTests
+{
+    internal static class ContextMother
+    {
+        internal static Mock<IExternalContext> CreateContext()
+        {
+            var context = new Mock<IExternalContext>();
+
+            context.Setup(c => c.Properties)
+                .Returns(new Dictionary<string, string>
+                {
+                    {ContextPropertyKeys.TransientDatabaseConnectionString, ConfigurationManager.AppSettings["TransientConnectionString"]},
+                    {ContextPropertyKeys.LogLevel, "DEBUG"}
+                });
+
+            return context;
+        }
+    }
+}
---
+++ src/PeriodEnd/Transfers/SFA.DAS.ProviderPayments.Calc.Transfers.UnitTests/TransfersTaskTests/WhenExecutingWithValidContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using AutoFixture.NUnit3;
+using CS.Common.External.Interfaces;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Payments.DCFS.Infrastructure.DependencyResolution;
+using SFA.DAS.ProviderPayments.Calc.Transfers.UnitTests.Utilities;
+
+namespace SFA.DAS.ProviderPayments.Calc.Transfers.UnitTests.TransfersTaskTests
+{
+    public class WhenExecutingWithValidContext
+    {
+        private Mock<IExternalContext> _context;
+        private Mock<TransfersProcessor> _processor;
+        private Mock<IDependencyResolver> _dependencyResolver;
+        private TransfersTask _task;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _context = ContextMother.CreateContext();
+
+            _processor = new Mock<TransfersProcessor>();
+
+            _dependencyResolver = new Mock<IDependencyResolver>();
+            _dependencyResolver.Setup(dr => dr.GetInstance<TransfersProcessor>())
+                .Returns(_processor.Object);
+
+            _task = new TransfersTask(_dependencyResolver.Object);
+        }
+
+        [Test]
+        public void ThenItShouldInitialiseTheDependencyResolver()
+        {
+            // Act
+            _task.Execute(_context.Object);
+
+            // Assert
+            _dependencyResolver.Verify(dr => dr.Init(It.Is<Type>(t=>t==typeof(TransfersProcessor)), It.IsAny<Payments.DCFS.Context.ContextWrapper>()), Times.Once);
+        }
+
+        [Test]
+        public void ThenItShouldExecuteTheProcessor()
+        {
+            //Act
+            _task.Execute(_context.Object);
+
+            // Assert
+            _processor.Verify(p => p.Process(), Times.Once);
+        }
+    }
+}
