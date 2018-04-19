1ba49e4ce667fdafd83ea7d1bdb0c3f57c0ac8c2
David Gouge <davegouge@gmail.com>
2018-04-19 09:01:07 +0100

    Moved Add Columns and Add Index into their own migration

--- src/SharedPipelineComponents/IlrSubmissionEvents/DeploySubmission/sql/migrationscripts/1_SubmissionEvents.Deds.Add_Indexes.sql
+++ src/SharedPipelineComponents/IlrSubmissionEvents/DeploySubmission/sql/migrationscripts/1_SubmissionEvents.Deds.Add_Indexes.sql
@@ -2,8 +2,3 @@ IF IndexProperty(Object_Id('Submissions.LastSeenVersion'), 'IX_LastSeenVersion_U
 BEGIN
 	CREATE INDEX IX_LastSeenVersion_UKPRN ON Submissions.LastSeenVersion (UKPRN, LearnRefNumber, PriceEpisodeIdentifier);
 END
-
-IF IndexProperty(Object_Id('Submissions.SubmissionEvents'), 'IX_ULN', 'IndexId') IS NULL
-BEGIN
-	CREATE INDEX IX_ULN ON Submissions.SubmissionEvents (ULN);
-END
--- src/SharedPipelineComponents/IlrSubmissionEvents/DeploySubmission/sql/migrationscripts/~2_Submissions.Deds.AddColumns.sql
+++ src/SharedPipelineComponents/IlrSubmissionEvents/DeploySubmission/sql/migrationscripts/~2_Submissions.Deds.AddColumns.sql
@@ -15,36 +15,3 @@ IF NOT EXISTS(SELECT [column_id] FROM sys.columns WHERE [name] = 'EPAOrgId' AND 
 	ADD [EPAOrgId] varchar(7) NULL
   END
 GO
-
--- filename starts with a ~ to ensure this alter table script runs *after* the table creation script
-
-IF NOT EXISTS(SELECT [column_id] FROM sys.columns WHERE [name] = 'GivenNames' AND [object_id] = OBJECT_ID('Submissions.LastSeenVersion'))
-  BEGIN
-    ALTER TABLE [Submissions].[LastSeenVersion]
-	ADD [GivenNames] varchar(100) NULL
-  END
-GO
-
---Submissions.SubmissionEvents
-IF NOT EXISTS(SELECT [column_id] FROM sys.columns WHERE [name] = 'GivenNames' AND [object_id] = OBJECT_ID('Submissions.SubmissionEvents'))
-  BEGIN
-    ALTER TABLE [Submissions].[SubmissionEvents]
-	ADD [GivenNames] varchar(100) NULL
-  END
-GO
-
--- filename starts with a ~ to ensure this alter table script runs *after* the table creation script
-IF NOT EXISTS(SELECT [column_id] FROM sys.columns WHERE [name] = 'FamilyName' AND [object_id] = OBJECT_ID('Submissions.LastSeenVersion'))
-  BEGIN
-    ALTER TABLE [Submissions].[LastSeenVersion]
-	ADD [FamilyName] varchar(100) NULL
-  END
-GO
-
---Submissions.SubmissionEvents
-IF NOT EXISTS(SELECT [column_id] FROM sys.columns WHERE [name] = 'FamilyName' AND [object_id] = OBJECT_ID('Submissions.SubmissionEvents'))
-  BEGIN
-    ALTER TABLE [Submissions].[SubmissionEvents]
-	ADD [FamilyName] varchar(100) NULL
-  END
-GO
