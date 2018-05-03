5ab42acf0d1748c92f9cbd40a8d75c460359eabe
Paul Graham <paul.graham@amido.com>
2018-05-03 13:42:28 +0100

    updated view to have the ON statments on a seperate line

--- src/PeriodEnd/LevyPayments/DeployLevyPayments/sql/ddl/PeriodEnd.Transient.LevyPayments.ddl.views.sql
+++ src/PeriodEnd/LevyPayments/DeployLevyPayments/sql/ddl/PeriodEnd.Transient.LevyPayments.ddl.views.sql
@@ -20,8 +20,10 @@ SELECT
 	acc.AccountName,
 	(acc.Balance - ISNULL(stat.LevySpent,0) - ISNULL(ta.Amount,0)) [Balance]
 FROM Reference.DasAccounts acc
-LEFT JOIN LevyPayments.AccountProcessStatus stat ON acc.AccountId = stat.AccountId
-LEFT JOIN TransferPayments.vw_TransferedAmountForAccount ta ON acc.AccountId = ta.FundingAccountId
+LEFT JOIN LevyPayments.AccountProcessStatus stat 
+	ON acc.AccountId = stat.AccountId
+LEFT JOIN TransferPayments.vw_TransferedAmountForAccount ta 
+	ON acc.AccountId = ta.FundingAccountId
 WHERE acc.AccountId IN (SELECT AccountId FROM Reference.DasCommitments)
 AND (stat.HasBeenProcessed IS NULL OR stat.HasBeenProcessed = 0)
 GO
