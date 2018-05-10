fda4375c482286bb2c7d6065309ecb6f881b2a97
tom-gough <tom@tgsd.co.uk>
2018-05-10 10:22:59 +0100

    Fixed naming of transfer allowance step methods

--- src/AcceptanceTesting/SpecByExample/SFA.DAS.Payments.AcceptanceTests/StepDefinitions/EmployerAccountSteps.cs
+++ src/AcceptanceTesting/SpecByExample/SFA.DAS.Payments.AcceptanceTests/StepDefinitions/EmployerAccountSteps.cs
@@ -80,13 +80,13 @@ namespace SFA.DAS.Payments.AcceptanceTests.StepDefinitions
         }
 
         [Given("the employer (.*) has a transfer allowance > agreed price for all months")]
-        public void GivenNamedEmployersTransferBalanceIsMoreThanPrice(int employerId)
+        public void GivenNamedEmployersTransferAllowanceIsMoreThanPrice(int employerId)
         {
             UpdateTransferAllowance(employerId, int.MaxValue);
         }
 
         [Given("the employer (.*) has a transfer allowance = 0 for all months")]
-        public void GivenNamedEmployersTransferBalanceIsZeroForAllMonths(int employerId)
+        public void GivenNamedEmployersTransferAllowanceIsZeroForAllMonths(int employerId)
         {
             UpdateTransferAllowance(employerId, 0m);
         }
