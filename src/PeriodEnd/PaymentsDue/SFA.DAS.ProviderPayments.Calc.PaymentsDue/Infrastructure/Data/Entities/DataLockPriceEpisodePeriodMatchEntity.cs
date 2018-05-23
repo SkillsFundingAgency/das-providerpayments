52ea3f61f7dd03c1f682eb914f32b882cedf6626
Scott Wakefield <scott@expertcc.co.uk>
2018-05-23 14:40:44 +0100

    DPP-1330 added datalocks entity.

---
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Infrastructure/Data/Entities/DataLockPriceEpisodePeriodMatchEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities
+{
+    public class DataLockPriceEpisodePeriodMatchEntity
+    {
+        public long Ukprn { get; set; }
+        [StringLength(25)]
+        public string PriceEpisodeIdentifier { get; set; }
+        [StringLength(12)]
+        public string LearnRefNumber { get; set; }
+        public int AimSeqNumber { get; set; }
+        public long CommitmentId { get; set; }
+        [StringLength(25)]
+        public string VersionId { get; set; }
+        public int Period { get; set; }
+        public bool Payable { get; set; }
+        public int TransactionType { get; set; }
+        public int TransactionTypesFlag { get; set; }
+    }
+}
