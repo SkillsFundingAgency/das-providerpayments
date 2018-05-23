5cc49c45ba398724aa568bce427794b7ea866c7c
Scott Wakefield <scott@expertcc.co.uk>
2018-05-23 15:38:49 +0100

    DPP-1330 added initial tests for data lock repo.

---
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue.IntegrationTests/Infrastructure/GivenADataLockPriceEpisodePeriodMatchesRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Payments.DCFS.Infrastructure.Data;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Repositories;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.IntegrationTests.Utilities;
+
+namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.IntegrationTests.Infrastructure
+{
+    [TestFixture, SetupUkprn]
+    public class GivenADataLockPriceEpisodePeriodMatchesRepository
+    {
+        private DataLockPriceEpisodePeriodMatchesRepository _sut;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _sut = new DataLockPriceEpisodePeriodMatchesRepository(GlobalTestContext.Instance.TransientConnectionString);
+        }
+
+        [TestFixture, SetupNoDataLockPriceEpisodePeriodMatches]
+        public class AndThereAreNoDataLocksForProvider : GivenADataLockPriceEpisodePeriodMatchesRepository
+        {
+            [TestFixture]
+            public class WhenCallingGetAllForProvider : AndThereAreNoDataLocksForProvider
+            {
+                [Test]
+                public void ThenItReturnsAnEmptyList()
+                {
+                    Setup();
+                    var result = _sut.GetAllForProvider(PaymentsDueTestContext.Ukprn);
+                    result.Should().BeEmpty();
+                }
+            }
+        }
+    }
+
+    public class DataLockPriceEpisodePeriodMatchesRepository: DcfsRepository
+    {
+        public DataLockPriceEpisodePeriodMatchesRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public List<DataLockPriceEpisodePeriodMatchEntity> GetAllForProvider(long ukprn)
+        {
+            return new List<DataLockPriceEpisodePeriodMatchEntity>();
+        }
+    }
+}
