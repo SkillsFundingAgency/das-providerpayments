1aea2dbdb8098ecf2c531e8bce1be6c8fa045111
Scott Wakefield <scott@expertcc.co.uk>
2018-06-01 13:39:10 +0100

    DPP-1330 added initial test for provider processor.

--- src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests/GivenAPaymentsDueProcessor.cs
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests/GivenAPaymentsDueProcessor.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections.Generic;
 using AutoFixture.NUnit3;
-using FluentAssertions;
 using Moq;
 using NUnit.Framework;
-using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Application;
 using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data;
 using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities;
-using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Repositories;
 using SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests.Utilities;
 
 namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests
@@ -40,50 +37,6 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests
             providers.ForEach(provider =>
                 mockProviderProcessor.Verify(processor =>
                     processor.Process(provider), Times.Once));
-        }
-
-        [Test, PaymentsDueAutoData, Ignore("for now")]
-        public void ThenItSavesRefunds(
-            ProviderEntity provider,
-            List<PayableEarning> payableEarnings,
-            List<RequiredPaymentsHistoryEntity> historicalPayments,
-            [Frozen] Mock<IProviderRepository> mockProviderRepository,
-            [Frozen] Mock<IPayableEarningsCalculator> mockPayableEarningsCalculator,
-            [Frozen] Mock<IRequiredPaymentsHistoryRepository> mockHistoricalPaymentsRepository,
-            [Frozen] Mock<IRequiredPaymentRepository> mockRequiredPaymentsRepository,
-            PaymentsDueProcessorV2 sut)
-        {
-            RequiredPaymentEntity[] actualRequiredPayments = {};
-            RequiredPaymentEntity[] expectedRequiredPayments = {};
-
-            payableEarnings.ForEach(earning =>
-            {
-                earning.Ukprn = provider.Ukprn;
-                earning.Uln = 4;//todo get from comparer poco
-            });
-
-            mockProviderRepository
-                .Setup(repository => repository.GetAllProviders())
-                .Returns(new[] {provider});
-
-            mockPayableEarningsCalculator
-                .Setup(calculator => calculator.Calculate(It.IsAny<long>()))
-                .Returns(payableEarnings);
-
-            mockHistoricalPaymentsRepository
-                .Setup(repository => repository.GetAllForProvider(It.IsAny<long>()))
-                .Returns(historicalPayments);
-
-            mockRequiredPaymentsRepository
-                .Setup(repository => repository.AddRequiredPayments(It.IsAny<RequiredPaymentEntity[]>()))
-                .Callback((RequiredPaymentEntity[] actualPaymentEntities) =>
-                {
-                    actualRequiredPayments = actualPaymentEntities;
-                });
-
-            sut.Process();
-
-            actualRequiredPayments.ShouldAllBeEquivalentTo(expectedRequiredPayments);
         }
     }
 }
---
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests/GivenAProviderProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Application;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests.Utilities;
+
+namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests
+{
+    [TestFixture]
+    public class GivenAProviderProcessor
+    {
+        [Test, PaymentsDueAutoData]
+        public void ThenItGetsLearnersForTheProvider(
+            ProviderEntity provider,
+            [Frozen] Mock<IProviderLearnersBuilder> mockLearnersBuilder,
+            ProviderProcessor sut)
+        {
+            sut.Process(provider);
+
+            mockLearnersBuilder.Verify(builder => builder.Build(provider.Ukprn), Times.Once);
+        }
+
+        [Test, PaymentsDueAutoData]
+        public void ThenItDoesSomethingForEachLearner(//todo:name
+            ProviderEntity provider,
+            Dictionary<string, Learner> learners,
+            [Frozen] Mock<IProviderLearnersBuilder> mockLearnersBuilder,
+            ProviderProcessor sut)
+        {
+            mockLearnersBuilder
+                .Setup(builder => builder.Build(It.IsAny<long>()))
+                .Returns(learners);
+
+            sut.Process(provider);
+
+            foreach (var learner in learners)
+            {
+                // todo: assert something is done
+            }
+        }
+    }
+}
