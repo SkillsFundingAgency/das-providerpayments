7b3cb35961671865301e8de3d8742adf176d3d96
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-06-04 15:59:53 +0100

    Updated learner with the properties required

--- src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Application/Learner.cs
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Application/Learner.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities;
 
 namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Application
@@ -10,5 +11,18 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Application
         public List<DataLockPriceEpisodePeriodMatchEntity> DataLocks { get; set; } = new List<DataLockPriceEpisodePeriodMatchEntity>();
         public List<RequiredPaymentsHistoryEntity> HistoricalPayments { get; set; } = new List<RequiredPaymentsHistoryEntity>();
         public List<RequiredPaymentEntity> RequiredPayments { get; set; } = new List<RequiredPaymentEntity>();
+
+        public List<string> DatalockErrors { get; private set; } = new List<string>();
+
+        private IEnumerable<RawEarningEntity> Act1RawEarnings => RawEarnings.Where(x => x.Act == 1);
+        private IEnumerable<RawEarningEntity> Act2RawEarnings => RawEarnings.Where(x => x.Act == 2);
+
+        public List<RawEarningEntity> PayableEarnings { get; set; }
+        public List<RawEarningEntity> NonPayableEarnings { get; set; }
+
+        public void ValidateDatalocks()
+        {
+
+        }
     }
 }
