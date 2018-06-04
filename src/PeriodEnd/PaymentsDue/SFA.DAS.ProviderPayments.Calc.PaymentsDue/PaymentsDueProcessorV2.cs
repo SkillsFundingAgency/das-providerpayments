42723acdbe9b03e85c67591c7bbdcc66629232f8
Scott Wakefield <scott@expertcc.co.uk>
2018-06-04 18:09:35 +0100

    DPP-1330 instrumentation of top level classes.

--- src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/PaymentsDueProcessorV2.cs
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/PaymentsDueProcessorV2.cs
@@ -1,28 +1,36 @@
-﻿using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data;
+﻿using NLog;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data;
 
 namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue
 {
     public class PaymentsDueProcessorV2
     {
+        private readonly ILogger _logger;
         private readonly IProviderRepository _providerRepository;
         private readonly IProviderProcessor _providerProcessor;
 
         public PaymentsDueProcessorV2(
+            ILogger logger,
             IProviderRepository providerRepository, 
             IProviderProcessor providerProcessor)
         {
+            _logger = logger;
             _providerRepository = providerRepository;
             _providerProcessor = providerProcessor;
         }
 
         public virtual void Process()
         {
+            _logger.Info("Started Payments Due Processor.");
+
             var providers = _providerRepository.GetAllProviders();
 
             foreach (var provider in providers)
             {
                 _providerProcessor.Process(provider);
             }
+
+            _logger.Info("Finished Payments Due Processor.");
         }
     }
 }
--- src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/ProviderProcessor.cs
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/ProviderProcessor.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Application;
+﻿using NLog;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Application;
 using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data;
 using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities;
 using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Repositories;
@@ -7,14 +8,18 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue
 {
     public class ProviderProcessor : IProviderProcessor
     {
+        private readonly ILogger _logger;
         private readonly IProviderLearnersBuilder _providerLearnersBuilder;
         private readonly INonPayableEarningRepository _nonPayableEarningRepository;
         private readonly IRequiredPaymentRepository _requiredPaymentRepository;
 
-        public ProviderProcessor(IProviderLearnersBuilder providerLearnersBuilder,
+        public ProviderProcessor(
+            ILogger logger,
+            IProviderLearnersBuilder providerLearnersBuilder,
             INonPayableEarningRepository nonPayableEarningRepository,
             IRequiredPaymentRepository requiredPaymentRepository)
         {
+            _logger = logger;
             _providerLearnersBuilder = providerLearnersBuilder;
             _nonPayableEarningRepository = nonPayableEarningRepository;
             _requiredPaymentRepository = requiredPaymentRepository;
@@ -22,16 +27,27 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue
 
         public void Process(ProviderEntity provider)
         {
+            _logger.Info($"Processing started for Provider UKPRN: [{provider.Ukprn}].");
+
             var providerLearners = _providerLearnersBuilder.Build(provider.Ukprn);
 
             foreach (var learner in providerLearners)
             {
+                _logger.Info($"Processing started for Learner LearnRefNumber: [{learner.Key}].");
+
                 // todo: learner data lock calcs
                 // todo: learner aggregate payment calcs
 
+                _logger.Info($"There are [{learner.Value.NonPayableEarnings.Count}] non-payable earnings for Learner LearnRefNumber: [{learner.Key}].");
                 _nonPayableEarningRepository.AddMany(learner.Value.NonPayableEarnings);
+
+                _logger.Info($"There are [{learner.Value.RequiredPayments.Count}] payable earnings for Learner LearnRefNumber: [{learner.Key}].");
                 _requiredPaymentRepository.AddRequiredPayments(learner.Value.RequiredPayments.ToArray());
+
+                _logger.Info($"Processing finished for Learner LearnRefNumber: [{learner.Key}].");
             }
+
+            _logger.Info($"Processing finished for Provider UKPRN: [{provider.Ukprn}].");
         }
     }
 }
