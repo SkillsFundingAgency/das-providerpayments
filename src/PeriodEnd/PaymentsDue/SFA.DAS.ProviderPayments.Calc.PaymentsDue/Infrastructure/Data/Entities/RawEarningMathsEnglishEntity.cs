5cd5ce1662740bdbf42736bbfa73f10344b103ed
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-06-05 11:09:52 +0100

    Updated field names for ME raw earnings

--- src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Infrastructure/Data/Entities/RawEarningMathsEnglishEntity.cs
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Infrastructure/Data/Entities/RawEarningMathsEnglishEntity.cs
@@ -10,21 +10,21 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities
         public long Ukprn { get; set; }
         public int AimSeqNumber { get; set; }
         [DataType(DataType.Date)]
-        public DateTime LearnStartDate { get; set; }
+        public DateTime LearningStartDate { get; set; }
         public int Period { get; set; }
         public long Uln { get; set; }
-        public int? ProgType { get; set; }
-        public int? FworkCode { get; set; }
-        public int? PwayCode { get; set; }
-        public int? StdCode { get; set; }
-        public decimal? LearnDelSfaContribPct { get; set; }
+        public int ProgrammeType { get; set; }
+        public int FrameworkCode { get; set; }
+        public int PathwayCode { get; set; }
+        public int StandardCode { get; set; }
+        public decimal SfaContributionPct { get; set; }
         [StringLength(100)]
-        public string FundLineType { get; set; }
+        public string FundingLineType { get; set; }
         [StringLength(8)]
         public string LearnAimRef { get; set; }
         public short Act { get; set; }
-        public decimal? TransactionType13 { get; set; }
-        public decimal? TransactionType14 { get; set; }
-        public decimal? TransactionType15 { get; set; }
+        public decimal TransactionType13 { get; set; }
+        public decimal TransactionType14 { get; set; }
+        public decimal TransactionType15 { get; set; }
     }
 }
