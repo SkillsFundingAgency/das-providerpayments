f605af13e9df2c88cdaf63674bd25a73ad2d7c86
Scott Wakefield <scott@expertcc.co.uk>
2018-06-06 14:43:50 +0100

    DPP-1330 added params and results types.

---
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Services/LearnerProcessParameters.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities;
+
+namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Services
+{
+    public class LearnerProcessParameters
+    {
+        public string LearnRefNumber { get; set; }
+        public List<RawEarning> RawEarnings { get; set; } = new List<RawEarning>();
+        public List<RawEarningForMathsOrEnglish> RawEarningsMathsEnglish { get; set; } = new List<RawEarningForMathsOrEnglish>();
+        public List<DataLockPriceEpisodePeriodMatchEntity> DataLocks { get; set; } = new List<DataLockPriceEpisodePeriodMatchEntity>();
+        public List<RequiredPaymentsHistoryEntity> HistoricalPayments { get; set; } = new List<RequiredPaymentsHistoryEntity>();
+        public List<RequiredPaymentEntity> RequiredPayments { get; set; } = new List<RequiredPaymentEntity>();
+        public List<Commitment> Commitments { get; set; } = new List<Commitment>();
+        public List<CollectionPeriodEntity> CollectionPeriods { get; set; } = new List<CollectionPeriodEntity>();
+    }
+}
---
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Services/LearnerProcessResults.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities;
+
+namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Services
+{
+    public class LearnerProcessResults
+    {
+        public List<RequiredPaymentEntity> PayableEarnings { get; set; } = new List<RequiredPaymentEntity>();
+        //public List<FundingDue> FundingDue { get; } = new List<FundingDue>();
+        public List<NonPayableEarningEntity> NonPayableEarnings { get; set; } = new List<NonPayableEarningEntity>();
+    }
+}
