3a3aeb167aada34250baafc6b2ac9bc08c7aa9b4
Scott Wakefield <scott@expertcc.co.uk>
2018-06-07 16:43:22 +0100

    DPP-1330 added test for when learner is ready.

--- src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests/GivenALearnerProcessor.cs
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests/GivenALearnerProcessor.cs
@@ -1,7 +1,10 @@
-﻿using AutoFixture.NUnit3;
+﻿using System.Collections.Generic;
+using AutoFixture.NUnit3;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Domain;
+using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities;
 using SFA.DAS.ProviderPayments.Calc.PaymentsDue.Services;
 using SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests.Utilities;
 
@@ -34,6 +37,7 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests
             [Test, PaymentsDueAutoData]
             public void ThenItCalculatesPaymentsFromLearner(
                 LearnerProcessParameters parameters,
+                List<PriceEpisode> priceEpisodes,
                 [Frozen] Mock<IIShouldBeInTheDataLockComponent> mockDataLockComponent,
                 [Frozen] Mock<IDataLockComponentFactory> mockDataLockFactory,
                 [Frozen] Mock<ILearner> mockLearner,
@@ -48,11 +52,42 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.UnitTests
                     .Setup(factory => factory.CreateLearner())
                     .Returns(mockLearner.Object);
 
+                mockDataLockComponent
+                    .Setup(component => component.ValidatePriceEpisodes(It.IsAny<List<Commitment>>(),
+                        It.IsAny<List<DataLockPriceEpisodePeriodMatchEntity>>()))
+                    .Returns(priceEpisodes);
+
                 sut.Process(parameters);
 
                 mockLearner
-                    .Verify(learner => learner.CalculatePaymentsDue(), 
+                    .Verify(learner => learner.CalculatePaymentsDue(), //todo: refactor to take params, inc priceEpisode from datalock components
                     Times.Once);
+            }
+
+            [Test, PaymentsDueAutoData, Ignore("till implemented todos")]
+            public void ThenItReturnsTheCalculationResult(
+                LearnerProcessParameters parameters,
+                List<PriceEpisode> priceEpisodes,
+                [Frozen] Mock<IIShouldBeInTheDataLockComponent> mockDataLockComponent,
+                [Frozen] Mock<IDataLockComponentFactory> mockDataLockFactory,
+                [Frozen] Mock<ILearner> mockLearner,
+                [Frozen] Mock<ILearnerFactory> mockLearnerFactory,
+                LearnerProcessor sut)
+            {
+                mockDataLockFactory
+                    .Setup(factory => factory.CreateDataLockComponent())
+                    .Returns(mockDataLockComponent.Object);
+
+                mockLearnerFactory
+                    .Setup(factory => factory.CreateLearner())
+                    .Returns(mockLearner.Object);
+
+                // todo: mockLearner.Result 
+
+                var result = sut.Process(parameters);
+
+                // todo: result.PayableEarnings.ShouldAllBeEquivalentTo(mockLearner.PayableEarnings);
+                // todo: result.NonPayableEarnings.ShouldAllBeEquivalentTo(mockLearner.NonPayableEarnings);
             }
         }
     }
