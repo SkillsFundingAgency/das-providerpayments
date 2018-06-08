19a130f844b8eb811f59fd46461a5647d8060f03
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-06-08 19:38:10 +0100

    Fixed failing unit test Fixed comments in the tests Fixed test logic (made sure tha the bit that said it had a datalock had a datalock etc)

--- src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Infrastructure/Data/Entities/RawEarning.cs
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Infrastructure/Data/Entities/RawEarning.cs
@@ -10,6 +10,7 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities
         [StringLength(12)]
         public string LearnRefNumber { get; set; }
         public long Uln { get; set; }
+        [Range(0, 20)]
         public int AimSeqNumber { get; set; }
         [StringLength(8)]
         public string LearnAimRef { get; set; }
@@ -19,16 +20,23 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities
         public DateTime? EpisodeStartDate { get; set; }
         [DataType(DataType.Date)]
         public DateTime? EpisodeEffectiveTnpStartDate { get; set; }
+        [Range(1, 14)]
         public int Period { get; set; }
+        [Range(0, 1000)]
         public int ProgrammeType { get; set; }
+        [Range(0, 1000)]
         public int FrameworkCode { get; set; }
+        [Range(0, 1000)]
         public int PathwayCode { get; set; }
+        [Range(0, 1000)]
         public int StandardCode { get; set; }
+        [Range(0.9, 1.0)]
         public decimal SfaContributionPercentage { get; set; }
         [StringLength(100)]
         public string FundingLineType { get; set; }
         [DataType(DataType.Date)]
         public DateTime LearningStartDate { get; set; }
+        [Range(1, 2)]
         public int ApprenticeshipContractType { get; set; }
 
         public decimal TransactionType01 { get; set; }
--- src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Infrastructure/Data/Entities/RequiredPaymentEntity.cs
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Infrastructure/Data/Entities/RequiredPaymentEntity.cs
@@ -50,23 +50,33 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities
         public long Uln { get; set; }
         [StringLength(12)]
         public string LearnRefNumber { get; set; }
+        [Range(0, 20)]
         public int AimSeqNumber { get; set; }
         public long Ukprn { get; set; }
         public DateTime IlrSubmissionDateTime { get; set; }
+        [Range(1, 12)]
         public int DeliveryMonth { get; set; }
+        [Range(2017, 2019)]
         public int DeliveryYear { get; set; }
+        [Range(1, 15)]
         public int TransactionType { get; set; }
         public decimal AmountDue { get; set; }
 
+        [Range(0, 1000)]
         public int StandardCode { get; set; }
+        [Range(0, 1000)]
         public int ProgrammeType { get; set; }
+        [Range(0, 1000)]
         public int FrameworkCode { get; set; }
+        [Range(0, 1000)]
         public int PathwayCode { get; set; }
 
+        [Range(1,2)]
         public int ApprenticeshipContractType { get; set; }
         [StringLength(25)]
         public string PriceEpisodeIdentifier { get; set; }
 
+        [Range(0.9, 1.0)]
         public decimal SfaContributionPercentage { get; set; }
         [StringLength(100)]
         public string FundingLineType { get; set; }
@@ -76,7 +86,9 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities
         public DateTime LearningStartDate { get; set; }
         [StringLength(8)]
         public string CollectionPeriodName { get; set; }
+        [Range(1, 12)]
         public int CollectionPeriodMonth { get; set; }
+        [Range(2017, 2019)]
         public int CollectionPeriodYear { get; set; }
     }
 }
