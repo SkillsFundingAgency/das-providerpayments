1139a14dca30d6851eaf05ff2427fec00722c5a6
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-06-11 10:42:54 +0100

    Updated cleanup script

--- src/PeriodEnd/PaymentsDue/DeployPaymentsDue/sql/dml/PeriodEnd.PaymentsDue.Cleanup.Deds.DML.sql
+++ src/PeriodEnd/PaymentsDue/DeployPaymentsDue/sql/dml/PeriodEnd.PaymentsDue.Cleanup.Deds.DML.sql
@@ -1,6 +1,18 @@
+DELETE FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[Earnings] E
+	WHERE EXISTS (
+		SELECT Id FROM PaymentsDue.RequiredPayments R
+		WHERE [Ukprn] IN (SELECT DISTINCT [Ukprn] FROM [Reference].[Providers])
+        AND [CollectionPeriodName] IN (SELECT '${YearOfCollection}-' + [Collection_Period] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
+		AND E.RequiredPaymentId = R.Id
+    )
+GO
+
 DELETE FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[RequiredPayments]
     WHERE [Ukprn] IN (SELECT DISTINCT [Ukprn] FROM [Reference].[Providers])
-        AND [CollectionPeriodName] IN (SELECT '${YearOfCollection}-' + [Collection_Period] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
-        AND [CollectionPeriodMonth] IN (SELECT [Period] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
-        AND [CollectionPeriodYear] IN (SELECT [Calendar_Year] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
+    AND [CollectionPeriodName] IN (SELECT '${YearOfCollection}-' + [Collection_Period] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
+GO
+
+DELETE FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[NonPayableEarnings]
+    WHERE [Ukprn] IN (SELECT DISTINCT [Ukprn] FROM [Reference].[Providers])
+    AND [CollectionPeriodName] IN (SELECT '${YearOfCollection}-' + [Collection_Period] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
 GO
