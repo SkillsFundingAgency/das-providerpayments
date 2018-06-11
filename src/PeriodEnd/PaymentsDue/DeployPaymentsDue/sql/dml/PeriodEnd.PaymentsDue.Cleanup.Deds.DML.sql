80631c18a13b0cc272affa385c69595fd0045eaf
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-06-11 10:57:13 +0100

    Updated cleanup script

--- src/PeriodEnd/PaymentsDue/DeployPaymentsDue/sql/dml/PeriodEnd.PaymentsDue.Cleanup.Deds.DML.sql
+++ src/PeriodEnd/PaymentsDue/DeployPaymentsDue/sql/dml/PeriodEnd.PaymentsDue.Cleanup.Deds.DML.sql
@@ -1,18 +1,17 @@
+declare @collectionPeriodName varchar(8) = (SELECT [Collection_Period_Name] FROM ${DAS_PeriodEnd.FQ}.dbo.Collection_Period_Mapping WHERE [Collection_Open] = 1)
+
 DELETE FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[Earnings] E
 	WHERE EXISTS (
 		SELECT Id FROM PaymentsDue.RequiredPayments R
-		WHERE [Ukprn] IN (SELECT DISTINCT [Ukprn] FROM [Reference].[Providers])
-        AND [CollectionPeriodName] IN (SELECT '${YearOfCollection}-' + [Collection_Period] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
+		WHERE [CollectionPeriodName] = @collectionPeriodName
 		AND E.RequiredPaymentId = R.Id
     )
 GO
 
 DELETE FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[RequiredPayments]
-    WHERE [Ukprn] IN (SELECT DISTINCT [Ukprn] FROM [Reference].[Providers])
-    AND [CollectionPeriodName] IN (SELECT '${YearOfCollection}-' + [Collection_Period] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
+    WHERE [CollectionPeriodName] = @collectionPeriodName
 GO
 
 DELETE FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[NonPayableEarnings]
-    WHERE [Ukprn] IN (SELECT DISTINCT [Ukprn] FROM [Reference].[Providers])
-    AND [CollectionPeriodName] IN (SELECT '${YearOfCollection}-' + [Collection_Period] FROM [PaymentsDue].[vw_CollectionPeriods] WHERE [Collection_Open] = 1)
+    WHERE [CollectionPeriodName] = @collectionPeriodName
 GO
