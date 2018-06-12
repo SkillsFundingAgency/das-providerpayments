5803fe2b7271d6a8a63007d7240c3f71e28c2f5b
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-06-12 13:37:24 +0100

    Refactored the names in IShouldBeInTheDatalockComponent Updated Learner to deal with datalock component output for multiple overlapping datalocks Added files that should have gone with prior checkin

--- src/PeriodEnd/PaymentsDue/DeployPaymentsDue/sql/dml/PeriodEnd.PaymentsDue.Cleanup.Deds.DML.sql
+++ src/PeriodEnd/PaymentsDue/DeployPaymentsDue/sql/dml/PeriodEnd.PaymentsDue.Cleanup.Deds.DML.sql
@@ -1,16 +1,14 @@
-declare @collectionPeriodName varchar(8) = (SELECT [Collection_Period_Name] FROM ${DAS_PeriodEnd.FQ}.dbo.Collection_Period_Mapping WHERE [Collection_Open] = 1)
+DECLARE @collectionPeriodName VARCHAR(8) = (SELECT [Collection_Period_Name] FROM ${DAS_PeriodEnd.FQ}.dbo.Collection_Period_Mapping WHERE [Collection_Open] = 1)
 
-DELETE FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[Earnings] E
-	WHERE EXISTS (
-		SELECT Id FROM PaymentsDue.RequiredPayments R
-		WHERE [CollectionPeriodName] = @collectionPeriodName
-		AND E.RequiredPaymentId = R.Id
+DELETE E FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[Earnings] E
+       WHERE EXISTS (
+              SELECT Id FROM PaymentsDue.RequiredPayments R
+              WHERE [CollectionPeriodName] = @collectionPeriodName
+              AND E.RequiredPaymentId = R.Id
     )
-GO
 
 DELETE FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[RequiredPayments]
     WHERE [CollectionPeriodName] = @collectionPeriodName
-GO
 
 DELETE FROM ${DAS_PeriodEnd.FQ}.[PaymentsDue].[NonPayableEarnings]
     WHERE [CollectionPeriodName] = @collectionPeriodName
