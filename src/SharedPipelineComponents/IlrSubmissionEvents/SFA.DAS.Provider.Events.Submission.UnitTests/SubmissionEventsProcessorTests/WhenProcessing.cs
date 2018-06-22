1f1859780306f1cc64f99759f5fa15d266eee74a
David Gouge <davegouge@gmail.com>
2018-06-22 12:05:06 +0100

    Fixed merge issue

--- src/SharedPipelineComponents/IlrSubmissionEvents/SFA.DAS.Provider.Events.Submission.UnitTests/SubmissionEventsProcessorTests/WhenProcessing.cs
+++ src/SharedPipelineComponents/IlrSubmissionEvents/SFA.DAS.Provider.Events.Submission.UnitTests/SubmissionEventsProcessorTests/WhenProcessing.cs
@@ -177,7 +177,6 @@ namespace SFA.DAS.Provider.Events.Submission.UnitTests.SubmissionEventsProcessor
             _mediator.Verify(m => m.Send(It.Is<WriteSubmissionEventCommand>(c => c.Events[1].OnProgrammeTotalPrice == updatedSubmissionChanged.OnProgrammeTotalPrice)));
             _mediator.Verify(m => m.Send(It.Is<WriteSubmissionEventCommand>(c => c.Events[1].CompletionTotalPrice == updatedSubmissionChanged.CompletionTotalPrice)));
             _mediator.Verify(m => m.Send(It.Is<WriteSubmissionEventCommand>(c => c.Events[1].NiNumber == null)));
-            _mediator.Verify(m => m.Send(It.Is<WriteSubmissionEventCommand>(c => c.Events[1].EPAOrgId == ilrForFirstSubmission.EPAOrgId)));
             _mediator.Verify(m => m.Send(It.Is<WriteSubmissionEventCommand>(c => c.Events[1].GivenNames == ilrForFirstSubmission.GivenNames)));
             _mediator.Verify(m => m.Send(It.Is<WriteSubmissionEventCommand>(c => c.Events[1].FamilyName == ilrForFirstSubmission.FamilyName)));
             _mediator.Verify(m => m.Send(It.Is<WriteSubmissionEventCommand>(c => c.Events[1].CompStatus == ilrForFirstSubmission.CompStatus)));
