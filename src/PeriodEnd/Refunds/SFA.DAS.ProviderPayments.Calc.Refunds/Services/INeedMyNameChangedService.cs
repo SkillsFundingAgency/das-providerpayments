49f90cc72d4da7919f54a443ceaac9d751b8e05c
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-07-03 14:56:21 +0100

    Very early work. Doesn't even compile

---
+++ src/PeriodEnd/Refunds/SFA.DAS.ProviderPayments.Calc.Refunds/Services/INeedMyNameChangedService.cs
@@ -0,0 +1,25 @@
+﻿using SFA.DAS.Payments.DCFS.Domain;
+
+namespace SFA.DAS.ProviderPayments.Calc.Refunds.Services
+{
+    class INeedMyNameChangedService
+    {
+        private PaymentEntity CreatePayment(RequiredPaymentEntity refund, decimal amount, int deliveryYear, int deliveryMonth, FundingSource fundingSource)
+        {
+            var payment = new Payment
+            {
+                DeliveryYear = deliveryYear,
+                DeliveryMonth = deliveryMonth,
+                Amount = amount,
+                RequiredPaymentId = refund.Id,
+                CollectionPeriodName = refund.CollectionPeriodName,
+                CollectionPeriodMonth = refund.CollectionPeriodMonth,
+                CollectionPeriodYear = refund.CollectionPeriodYear,
+                FundingSource = fundingSource,
+                TransactionType = refund.TransactionType,
+            };
+
+            return payment;
+        }
+    }
+}
