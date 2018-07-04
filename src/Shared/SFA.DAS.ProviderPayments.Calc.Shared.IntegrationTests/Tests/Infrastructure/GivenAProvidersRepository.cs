9b1c6dd8f69bbe8cbd11548d84ff6be053e4d2ba
Paul Graham <paul.graham@coprime.co.uk>
2018-07-04 15:17:06 +0100

    Added Repo for Providers. And the int tests needed

---
+++ src/Shared/SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests/Tests/Infrastructure/GivenAProvidersRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data.Repositories;
+using SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests.Attributes;
+
+namespace SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests.Tests.Infrastructure
+{
+    [TestFixture, SetupProviderRepository(2)]
+    public class GivenAProvidersRepository
+    {
+        private ProviderRepository _sut;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _sut = new ProviderRepository(GlobalTestContext.Instance.TransientConnectionString);
+        }
+
+        [Test]
+        public void ThenReturnsAPopulatedList()
+        {
+            var result = _sut.GetAllProviders();
+            result.Count().Should().Be(2);
+        }
+
+        [Test]
+        public void ThenTheUkprnsMatch()
+        {
+            var result = _sut.GetAllProviders().OrderBy(x=>x.Ukprn).ToList();
+            result.First().Ukprn.Should().Be(1);
+            result.Last().Ukprn.Should().Be(2);
+        }
+    }
+}
