c82979fe174907af3932dd6ed6adc4c38543519b
Paul Graham <paul.graham@coprime.co.uk>
2018-07-05 14:29:30 +0100

    Added implementation of SummariseAccountBalances

---
+++ src/PeriodEnd/Refunds/SFA.DAS.ProviderPayments.Calc.Refunds/Services/SummariseAccountBalances.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using NLog;
+using SFA.DAS.ProviderPayments.Calc.Refunds.Dto;
+using SFA.DAS.ProviderPayments.Calc.Refunds.Services.Dependencies;
+using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data;
+using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data.Entities;
+
+namespace SFA.DAS.ProviderPayments.Calc.Refunds.Services
+{
+    public class SummariseAccountBalances : ISummariseAccountBalances
+    {
+        private ConcurrentDictionary<long, decimal> _accountsDictionary = new ConcurrentDictionary<long, decimal>();
+        public void IncrementAccountLevyBalance(IEnumerable<PaymentEntity> refunds)
+        {
+
+        }
+
+        public List<AccountLevyCredit> AsList()
+        {
+            return _accountsDictionary.Select(x=>new AccountLevyCredit {AccountId = x.Key, LevyCredit = x.Value}).ToList();
+        }
+    }
+}
