7d99d1da0e3c2e854478a968cc11ff4fa6cb226c
Paul Graham <paul.graham@coprime.co.uk>
2018-07-09 14:47:24 +0100

    Added DAS Service and the associated Unit Tests

---
+++ src/PeriodEnd/Refunds/SFA.DAS.ProviderPayments.Calc.Refunds.UnitTests/GivenADasAccountService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ProviderPayments.Calc.Refunds.Dto;
+using SFA.DAS.ProviderPayments.Calc.Refunds.Services;
+using SFA.DAS.ProviderPayments.Calc.Refunds.UnitTests.Utilities;
+using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data;
+
+namespace SFA.DAS.ProviderPayments.Calc.Refunds.UnitTests
+{
+    [TestFixture]
+    public class GivenADasAccountService
+    {
+        [Test, RefundsAutoData]
+        public void ThenItProcessesEachCredit(
+            List<AccountLevyCredit> credits,
+            [Frozen]Mock<IDasAccountRepository> dasAccountRepository,
+            DasAccountService sut
+            )
+        {
+            sut.UpdateAccountLevyBalances(credits);
+            dasAccountRepository.Verify(x=>x.UpdateBalance(It.IsAny<long>(), It.IsAny<decimal>()), Times.Exactly(3));
+        }
+
+        [Test, RefundsAutoData]
+        public void ThenItProcessesEachCredit(
+            AccountLevyCredit credit,
+            [Frozen]Mock<IDasAccountRepository> dasAccountRepository,
+            DasAccountService sut
+        )
+        {
+            sut.UpdateAccountLevyBalances(new List<AccountLevyCredit>{credit});
+            dasAccountRepository.Verify(x => x.UpdateBalance(credit.AccountId, credit.LevyCredit), Times.Once);
+        }
+    }
+}
