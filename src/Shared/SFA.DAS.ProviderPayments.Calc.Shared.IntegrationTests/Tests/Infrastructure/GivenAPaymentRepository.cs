493b990ee5308cafdbf740cc21012634010b946f
Scott Wakefield <scott@expertcc.co.uk>
2018-07-11 17:39:29 +0100

    DPP-1516 added datahelper and initial test.

--- src/Shared/SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests/Tests/Infrastructure/GivenAPaymentRepository.cs
+++ src/Shared/SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests/Tests/Infrastructure/GivenAPaymentRepository.cs
@@ -1,5 +1,12 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data;
+using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data.Entities;
 using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data.Repositories;
+using SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests.Helpers;
 
 namespace SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests.Tests.Infrastructure
 {
@@ -8,10 +15,37 @@ namespace SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests.Tests.Infrastruc
     {
         private PaymentRepository _sut;
 
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _sut = new PaymentRepository(GlobalTestContext.Instance.TransientConnectionString);
+        }
+
         [TestFixture]
         public class WhenCallingAddMany : GivenAPaymentRepository
         {
-            
+            private List<PaymentEntity> _expectedEntities;	
+            private List<PaymentEntity> _actualEntities;
+
+            [OneTimeSetUp]
+            public void Setup()
+            {
+                _expectedEntities = new Fixture()
+                    .Build<PaymentEntity>()
+                    .CreateMany()
+                    .ToList();
+
+                PaymentDataHelper.Truncate(PaymentSchema.Refunds);
+
+                _sut.AddMany(_expectedEntities, PaymentSchema.Refunds);
+
+                _actualEntities = PaymentDataHelper.GetAll(PaymentSchema.Refunds).ToList();	
+            }
+
+            [Test]	
+            public void ThenItSavesTheExpectedNumberOfEntities() =>
+                _actualEntities.Count
+                    .Should().Be(_expectedEntities.Count);
         }
     }
 }
