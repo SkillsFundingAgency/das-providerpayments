aa9fa8c1d7149d35f37384564f1ed90fbb565d68
Scott Wakefield <scott@expertcc.co.uk>
2018-07-11 16:27:34 +0100

    DPP-1516 early structure of tests.

---
+++ src/Shared/SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests/Tests/Infrastructure/GivenAPaymentRepository.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data.Repositories;
+
+namespace SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests.Tests.Infrastructure
+{
+    [TestFixture]
+    public class GivenAPaymentRepository
+    {
+        private PaymentRepository _sut;
+
+        [TestFixture]
+        public class WhenCallingAddMany : GivenAPaymentRepository
+        {
+            
+        }
+    }
+}
