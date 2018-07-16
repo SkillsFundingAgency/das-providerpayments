d2fb38147ede29719a3cc808ad601b6c8df82aca
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-07-16 15:13:32 +0100

    Updated transaction type handling to base it on the TransactionType enum Updated Commitments repo to directly look up the information that is required Removed some usings

--- src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Infrastructure/Data/Entities/Commitment.cs
+++ src/PeriodEnd/PaymentsDue/SFA.DAS.ProviderPayments.Calc.PaymentsDue/Infrastructure/Data/Entities/Commitment.cs
@@ -5,18 +5,22 @@ namespace SFA.DAS.ProviderPayments.Calc.PaymentsDue.Infrastructure.Data.Entities
 {
     public class Commitment : IHoldCommitmentInformation
     {
-        public long CommitmentId { get; set; }
-        public string CommitmentVersionId { get; set; }
         public long Uln { get; set; }
         public long Ukprn { get; set; }
+
+        public long CommitmentId { get; set; }
+        public string CommitmentVersionId { get; set; }
         public long AccountId { get; set; }
         public string AccountVersionId { get; set; }
+
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+
         public int StandardCode { get; set; }
         public int ProgrammeType { get; set; }
         public int FrameworkCode { get; set; }
         public int PathwayCode { get; set; }
+
         public int PaymentStatus { get; set; }
         public int Priority { get; set; }
         public DateTime EffectiveFrom { get; set; }
