8b73e11190d842c4f5b2248b75e20168b4d01032
Scott Wakefield <scott@expertcc.co.uk>
2018-07-19 14:16:33 +0100

    DPP-1514 added test structure for new account-legalentity repo.

---
+++ src/Reference/Accounts/SFA.DAS.Payments.Reference.Accounts.IntegrationTests/Infrastructure/Data/GivenAnAccountLegalEntityRepository.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace SFA.DAS.Payments.Reference.Accounts.IntegrationTests.Infrastructure.Data
+{
+    [TestFixture, Ignore("for now")]
+    public class GivenAnAccountLegalEntityRepository
+    {
+        
+    }
+}
