5a08f74461520a2037e44198bf4b8217e94ebfff
Scott Wakefield <scott@expertcc.co.uk>
2018-07-20 11:24:36 +0100

    DPP-1514 added initial test for api handler.

---
+++ src/Reference/Accounts/SFA.DAS.Payments.Reference.Accounts.UnitTests/Application/GetPageOfAccountLegalEntitiesQuery/GivenAGetPageOfAccountLegalEntitiesQueryHandler.cs
@@ -0,0 +1,52 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EAS.Account.Api.Client;
+using SFA.DAS.EAS.Account.Api.Types;
+using SFA.DAS.Payments.Reference.Accounts.Application.GetPageOfAccountLegalEntitiesQuery;
+
+namespace SFA.DAS.Payments.Reference.Accounts.UnitTests.Application.GetPageOfAccountLegalEntitiesQuery
+{
+    [TestFixture]
+    public class GivenAGetPageOfAccountLegalEntitiesQueryHandler
+    {
+        [TestFixture]
+        public class WhenCallingHandle
+        {
+            [Test, AccountsAutoData]
+            public void ThenItShouldReturnModelsFromApi(
+                GetPageOfAccountLegalEntitiesQueryRequest request,
+                PagedApiResponseViewModel<AccountLegalEntityViewModel> apiResponse,
+                [Frozen] Mock<IAccountApiClient> mockApi,
+                GetPageOfAccountLegalEntitiesQueryHandler sut)
+            {
+                mockApi
+                    .Setup(client => client.GetPageOfAccountLegalEntities(request.PageNumber, 1000))
+                    .ReturnsAsync(apiResponse);
+
+                var response = sut.Handle(request);
+
+                response.Items.Should().BeEquivalentTo(apiResponse.Data);
+            }
+
+            [Test]
+            public void AndMorePages_ThenHasMorePagesTrue()
+            {
+                
+            }
+
+            [Test]
+            public void AndNoPages_ThenHasMorePagesFalse()
+            {
+
+            }
+
+            [Test]
+            public void AndException_ThenReturnsIsValidFalse()
+            {
+                
+            }
+        }
+    }
+}
--- src/Reference/Accounts/SFA.DAS.Payments.Reference.Accounts/Application/GetPageOfAccountLegalEntitiesQuery/GetPageOfAccountLegalEntitiesQueryHandler.cs
+++ src/Reference/Accounts/SFA.DAS.Payments.Reference.Accounts/Application/GetPageOfAccountLegalEntitiesQuery/GetPageOfAccountLegalEntitiesQueryHandler.cs
@@ -1,14 +1,33 @@
-﻿using System;
-using MediatR;
+﻿using MediatR;
+using SFA.DAS.EAS.Account.Api.Client;
 
 namespace SFA.DAS.Payments.Reference.Accounts.Application.GetPageOfAccountLegalEntitiesQuery
 {
     public class GetPageOfAccountLegalEntitiesQueryHandler : 
         IRequestHandler<GetPageOfAccountLegalEntitiesQueryRequest, GetPageOfAccountLegalEntitiesQueryResponse>
     {
+        private const int PageSize = 1000;
+        private readonly IAccountApiClient _accountApiClient;
+
+        public GetPageOfAccountLegalEntitiesQueryHandler(IAccountApiClient accountApiClient)
+        {
+            _accountApiClient = accountApiClient;
+        }
+
         public GetPageOfAccountLegalEntitiesQueryResponse Handle(GetPageOfAccountLegalEntitiesQueryRequest message)
         {
-            throw new NotImplementedException();
+            var pagedApiResponse = AsyncHelpers.RunSync(async () =>
+            {
+                var result = await _accountApiClient.GetPageOfAccountLegalEntities(message.PageNumber, PageSize);
+                return result;
+            });
+
+            return new GetPageOfAccountLegalEntitiesQueryResponse
+            {
+                /*IsValid = true,
+                HasMorePages = pageOfAccounts.Page < pageOfAccounts.TotalPages,*/
+                Items = pagedApiResponse.Data.ToArray()
+            };
         }
     }
 }
