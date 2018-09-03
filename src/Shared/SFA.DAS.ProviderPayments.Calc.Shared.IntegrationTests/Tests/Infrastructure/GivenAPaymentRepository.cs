fc66d7de69681f908e1b7e613ac8dda1fcbb71c9
Paul Graham <paul.graham@coprime.co.uk>
2018-09-03 10:45:27 +0100

    Merged in shared Repo stuff for getting historic payments

--- src/Shared/SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests/Tests/Infrastructure/GivenAPaymentRepository.cs
+++ src/Shared/SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests/Tests/Infrastructure/GivenAPaymentRepository.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data;
 using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data.Entities;
 using SFA.DAS.ProviderPayments.Calc.Shared.Infrastructure.Data.Repositories;
+using SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests.Attributes;
 using SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests.Helpers;
 
 namespace SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests.Tests.Infrastructure
@@ -27,6 +28,25 @@ namespace SFA.DAS.ProviderPayments.Calc.Shared.IntegrationTests.Tests.Infrastruc
             public void SetupForRefunds()
             {
                 SetupForSchema(PaymentSchema.Refunds);
+            }
+        }
+
+
+        [SetupPaymentsHistoryRepository(1007)]
+        public class WhenCallingGetHistoricPayments : GivenAPaymentRepository
+        {
+            [Test]
+            public void ThenItReturnsHistoricPaymentsForMatchingProvider()
+            {
+                var result = _sut.GetHistoricEmployerPaymentsEachRoundedDownForProvider(1007);
+                result.Count().Should().Be(3);
+                result.First().Amount.Should().Be(300);
+            }
+            [Test]
+            public void ThenItReturnsNoHistoricPaymentsForNonMatchingProvider()
+            {
+                var result = _sut.GetHistoricEmployerPaymentsEachRoundedDownForProvider(2001);
+                result.Count().Should().Be(0);
             }
         }
 
