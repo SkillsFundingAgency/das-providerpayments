ddef44007cb0992491c29fdd445c19a9b7704a07
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-09-21 15:16:10 +0100

    Revert the revert

---
+++ src/SharedPipelineComponents/Datalock/SFA.DAS.CollectionEarnings.DataLock/Domain/PriceEpisode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace SFA.DAS.CollectionEarnings.DataLock.Domain
+{
+    class PriceEpisode
+    {
+        public PriceEpisode(string priceEpisodeIdentifier)
+        {
+            var datePart = priceEpisodeIdentifier.Substring(priceEpisodeIdentifier.Length - 10);
+            var date = DateTime.ParseExact(datePart, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            var firstDayOfAcademicYear = FirstDayOfAcademicYear(date);
+            var firstYearForAcademicYear = firstDayOfAcademicYear.Year % 100;
+            AcademicYear = $"{firstYearForAcademicYear}{firstYearForAcademicYear + 1}";
+        }
+
+        public string AcademicYear { get; set; }
+
+        DateTime FirstDayOfAcademicYear(DateTime source)
+        {
+            if (source.Month < 8)
+            {
+                return new DateTime(source.Year - 1, 8, 1);
+            }
+            return new DateTime(source.Year, 8, 1);
+        }
+    }
+}
