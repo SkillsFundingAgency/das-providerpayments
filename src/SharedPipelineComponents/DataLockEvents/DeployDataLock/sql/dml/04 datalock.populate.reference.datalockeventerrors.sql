b957de2d2f01b970dffeed2c032c67ecd0d20218
Carl Berisford-Murray <carl@techcellence.org.uk>
2018-11-05 11:37:26 +0000

    Updated sql files to match production

--- src/SharedPipelineComponents/DataLockEvents/DeployDataLock/sql/dml/04 datalock.populate.reference.datalockeventerrors.sql
+++ src/SharedPipelineComponents/DataLockEvents/DeployDataLock/sql/dml/04 datalock.populate.reference.datalockeventerrors.sql
@@ -3,16 +3,10 @@
 ---------------------------------------------------------------
 INSERT INTO Reference.DataLockEventErrors
 (DataLockEventId, ErrorCode, SystemDescription)
-SELECT 
-	dlee.DataLockEventId,
-    dlee.ErrorCode,
-    dlee.SystemDescription
-FROM OPENQUERY(${DAS_ProviderEvents.servername}, '
-	SELECT 
-		dlee.DataLockEventId, 
-		dlee.ErrorCode, 
-		dlee.SystemDescription
-	 FROM 
-		${DAS_ProviderEvents.databasename}.DataLock.DataLockEventErrors dlee'
-    ) AS dlee
-INNER JOIN Reference.DataLockEvents dle ON dle.DataLockEventId = dlee.DataLockEventId
+SELECT
+	dlee.DataLockEventId, 
+	dlee.ErrorCode, 
+	dlee.SystemDescription
+FROM ${DAS_ProviderEvents.FQ}.DataLock.DataLockEventErrors dlee
+INNER JOIN Reference.DataLockEvents dle 
+On dle.DataLockEventId = dlee.DataLockEventId 
